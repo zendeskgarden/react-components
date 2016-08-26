1f54a6678ec759158bb67b0e38196aa80b9dfcf8
Sune Simonsen <sune@we-knowhow.dk>
2016-08-26 13:45:56 +0200

    Make the Text and View Stateless components

--- src/core/View/index.js
+++ src/core/View/index.js
@@ -1,4 +1,4 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import classNames from 'classnames'
 
 import styles from './styles.css'
@@ -7,91 +7,89 @@ const hasAnyHandlers = (handlers) => (
   Object.keys(handlers).some((key) => handlers[key])
 )
 
-export default class View extends Component {
-  static propTypes = {
-    autoFocus: PropTypes.bool,
-    children: PropTypes.node,
-    className: PropTypes.string,
-    dir: PropTypes.oneOf([ 'ltr', 'rtl' ]),
-    hidden: PropTypes.bool,
-    onClick: PropTypes.func,
-    onDragEnter: PropTypes.func,
-    onDragLeave: PropTypes.func,
-    onDragOver: PropTypes.func,
-    onDragStart: PropTypes.func,
-    onDrop: PropTypes.func,
-    onArrowDown: PropTypes.func,
-    onArrowLeft: PropTypes.func,
-    onArrowRight: PropTypes.func,
-    onArrowUp: PropTypes.func,
-    onDelete: PropTypes.func,
-    onEnter: PropTypes.func,
-    onEscape: PropTypes.func,
-    onKeyDown: PropTypes.func,
-    onSpace: PropTypes.func,
-    testId: PropTypes.string,
-    title: PropTypes.string
+const View = ({
+  children,
+  className,
+  hidden,
+  onArrowDown,
+  onArrowLeft,
+  onArrowRight,
+  onArrowUp,
+  onDelete,
+  onEnter,
+  onEscape,
+  onKeyDown,
+  onSpace,
+  testId,
+  ...other
+}) => {
+  const keyDownHandlers = {
+    '8': onDelete,
+    '13': onEnter,
+    '27': onEscape,
+    '32': onSpace,
+    '37': onArrowLeft,
+    '38': onArrowUp,
+    '39': onArrowRight,
+    '40': onArrowDown
   }
 
-  render () {
-    const {
-      children,
-      className,
-      hidden,
-      onArrowDown,
-      onArrowLeft,
-      onArrowRight,
-      onArrowUp,
-      onDelete,
-      onEnter,
-      onEscape,
-      onKeyDown,
-      onSpace,
-      testId,
-      ...other
-    } = this.props
+  const eventHandlers = {}
 
-    const keyDownHandlers = {
-      '8': onDelete,
-      '13': onEnter,
-      '27': onEscape,
-      '32': onSpace,
-      '37': onArrowLeft,
-      '38': onArrowUp,
-      '39': onArrowRight,
-      '40': onArrowDown
+  if (onKeyDown || hasAnyHandlers(keyDownHandlers)) {
+    eventHandlers.onKeyDown = (e) => {
+      const handler = keyDownHandlers[e.keyCode]
+      handler && handler(e)
+      onKeyDown && onKeyDown(e)
     }
-
-    const eventHandlers = {}
-
-    if (onKeyDown || hasAnyHandlers(keyDownHandlers)) {
-      eventHandlers.onKeyDown = (e) => {
-        const handler = keyDownHandlers[e.keyCode]
-        handler && handler(e)
-        onKeyDown && onKeyDown(e)
-      }
-    }
-
-    const props = {
-      ...other,
-      ...eventHandlers
-    }
-
-    if (testId) {
-      props['data-test-id'] = testId
-    }
-
-    if (hidden) {
-      props['aria-hidden'] = true
-    }
-
-    return (
-      <div
-        { ...props }
-        className={ classNames(styles.view, className) }
-      >
-        { children }
-      </div>
-    )
   }
+
+  const newProps = {
+    ...other,
+    ...eventHandlers
+  }
+
+  if (testId) {
+    newProps['data-test-id'] = testId
+  }
+
+  if (hidden) {
+    newProps['aria-hidden'] = true
+  }
+
+  return (
+    <div
+      { ...newProps }
+      className={ classNames(styles.view, className) }
+    >
+      { children }
+    </div>
+  )
 }
+
+View.propTypes = {
+  autoFocus: PropTypes.bool,
+  children: PropTypes.node,
+  className: PropTypes.string,
+  dir: PropTypes.oneOf([ 'ltr', 'rtl' ]),
+  hidden: PropTypes.bool,
+  onClick: PropTypes.func,
+  onDragEnter: PropTypes.func,
+  onDragLeave: PropTypes.func,
+  onDragOver: PropTypes.func,
+  onDragStart: PropTypes.func,
+  onDrop: PropTypes.func,
+  onArrowDown: PropTypes.func,
+  onArrowLeft: PropTypes.func,
+  onArrowRight: PropTypes.func,
+  onArrowUp: PropTypes.func,
+  onDelete: PropTypes.func,
+  onEnter: PropTypes.func,
+  onEscape: PropTypes.func,
+  onKeyDown: PropTypes.func,
+  onSpace: PropTypes.func,
+  testId: PropTypes.string,
+  title: PropTypes.string
+}
+
+export default View
