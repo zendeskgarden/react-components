1f919c0a05656d761a48c99ad7c8ea473a0ea127
Albert Fernández <albert@nandez.cat>
2016-12-15 12:11:14 +0100

    Simplify example and remove unnecessary dependencies

--- example/src/index.js
+++ example/src/index.js
@@ -3,15 +3,17 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 
-import styles from './styles.css'
-
-import { Button } from 'zd-react-components'
+import { Button, Text } from 'zd-react-components'
+import './styles.css'
 
 var root = document.createElement('div')
 document.body.appendChild(root)
 
 render(
-  <div className={ styles.app }>
+  <div className='app'>
+    <div className='header'>
+      <Text className='title'>Zendesk Garden - React Components</Text>
+    </div>
     <Button onClick={() => window.alert('Hello world!')}>Hello</Button>
   </div>,
   root
