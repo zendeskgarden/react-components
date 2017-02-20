b1ad06da7cdcd9c770d3c1bb069d549443c01301
Sune Simonsen <sune@we-knowhow.dk>
2017-02-20 11:09:50 +0100

    Support testing the styleguide without Proxima-nova
    You can disable Proxima-nova by supplying `?font=system` in the styleguide.

--- src/styleguide/Wrapper.js
+++ src/styleguide/Wrapper.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react'
 import ThemeProvider from '../utils/theming/ThemeProvider'
 import exampleTheme from '../themes/example-theme'
 import electroidDarkTheme from '../themes/electroid-dark-theme'
+import querystring from 'querystring'
 
 export default class Wrapper extends Component {
   static propTypes = {
@@ -14,6 +15,9 @@ export default class Wrapper extends Component {
     } = this.props
 
     const theme = window.localStorage.getItem('rc-theme') || 'default'
+
+    const query = querystring.parse(window.location.search.slice(1))
+    document.body.classList.toggle('u-font-family-system', query.font === 'system')
 
     switch (theme) {
       case 'example':
