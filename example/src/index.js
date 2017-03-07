814d7274936968d50d79951df528b764d2af7035
Sune Simonsen <sune@we-knowhow.dk>
2017-03-07 09:56:19 +0100

    Updated the example to use the new build

--- example/src/index.js
+++ example/src/index.js
@@ -3,7 +3,11 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Button, Text } from 'zd-react-components'
+// Will not treeshake yet :-(
+// But you can use babel-plugin-transform-imports
+// to transform these imports to individual import.
+import {Button, Menu, Text} from 'zd-react-components'
+
 import './styles.css'
 
 var root = document.createElement('div')
@@ -14,7 +18,17 @@ render(
     <div className='header'>
       <Text className='title'>Zendesk Garden - React Components</Text>
     </div>
-    <Button onClick={() => window.alert('Hello world!')}>Hello</Button>
+    <Menu
+      trigger={<Button>Menu</Button>}
+      onSelect={(value) => window.alert(value)}
+    >
+      <Menu.Item value='profile'>Profile</Menu.Item>
+      <Menu.Item value='settings'>Settings</Menu.Item>
+      <Menu.Item value='theme editor' disabled>Theme Editor</Menu.Item>
+      <Menu.Separator />
+      <Menu.Item value='article editor'>Article Editor</Menu.Item>
+      <Menu.Item value='sign out'>Sign Out</Menu.Item>
+    </Menu>
   </div>,
   root
 )
