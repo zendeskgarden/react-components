c589fe4d8eae65ffc430db9ec4ffe7444faa5a27
Marc Høegh <mhoegh90@gmail.com>
2017-08-21 11:22:48 +0200

    TextArea: Rename onChangeText

--- src/TextArea/index.js
+++ src/TextArea/index.js
@@ -31,7 +31,7 @@ export default class TextArea extends ThemedComponent {
     onArrowUp: PropTypes.func,
     onBlur: PropTypes.func,
     onClick: PropTypes.func,
-    onChangeText: PropTypes.func,
+    onChange: PropTypes.func,
     onDelete: PropTypes.func,
     onEnter: PropTypes.func,
     onEscape: PropTypes.func,
