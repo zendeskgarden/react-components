fd693c73b9ddb689bab1f3a12378061359fc3da3
Austin Green <agreen@zendesk.com>
2017-10-16 10:44:51 -0700

    Theming: resetTheme addition to ThemeProvider

--- src/utils/theming/ThemedComponent/index.js
+++ src/utils/theming/ThemedComponent/index.js
@@ -1,30 +1,44 @@
 import { Component } from "react";
 import PropTypes from "prop-types";
 
-const extendStyles = (styles, theme, namespace) => {
+const extendStyles = (styles, theme, resetTheme, namespace) => {
   const { [namespace]: themeStyles } = theme || {};
 
   if (themeStyles) {
-    const extendStyles = {};
+    // If resetTheme is provided we don't want to override the default styling
+    if (!resetTheme) {
+      const extendStyles = {};
 
-    Object.keys(styles).forEach(key => {
-      extendStyles[key] =
-        key in themeStyles ? `${styles[key]} ${themeStyles[key]}` : styles[key];
-    });
+      Object.keys(styles).forEach(key => {
+        extendStyles[key] =
+          key in themeStyles
+            ? `${styles[key]} ${themeStyles[key]}`
+            : styles[key];
+      });
 
-    return extendStyles;
-  } else {
+      return extendStyles;
+    }
+
+    return themeStyles;
+  }
+
+  if (!resetTheme) {
     return styles;
   }
+
+  return {};
 };
 
 export default class ThemedComponent extends Component {
   static contextTypes = {
-    rcTheme: PropTypes.object
+    rcTheme: PropTypes.object,
+    resetTheme: PropTypes.bool
   };
 
   constructor(props, context, { namespace, styles }) {
     super(props, context);
-    this.theme = extendStyles(styles, context.rcTheme, namespace);
+
+    const { rcTheme, resetTheme } = context;
+    this.theme = extendStyles(styles, rcTheme, resetTheme, namespace);
   }
 }
