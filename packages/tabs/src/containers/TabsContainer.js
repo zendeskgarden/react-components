fb3100fbd70ceca5d3e37dade1632c634bc13ed7
Jonathan Zempel <jzempel@gmail.com>
2018-05-08 19:25:29 -0700

    Lint JS

--- packages/tabs/src/containers/TabsContainer.js
+++ packages/tabs/src/containers/TabsContainer.js
@@ -7,11 +7,7 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import {
-  SelectionContainer,
-  ControlledComponent,
-  IdManager
-} from '@zendeskgarden/react-selection';
+import { SelectionContainer, ControlledComponent, IdManager } from '@zendeskgarden/react-selection';
 
 export default class TabsContainer extends ControlledComponent {
   static propTypes = {
