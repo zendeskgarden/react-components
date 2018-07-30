9d8f8fdb6a4a425b26991994d29ef08d9dc3d197
Jonathan Zempel <jzempel@gmail.com>
2018-07-30 13:51:57 -0700

    Add `ChevronButton` export

--- packages/buttons/src/index.js
+++ packages/buttons/src/index.js
@@ -9,6 +9,7 @@ export { default as ButtonGroupContainer } from './containers/ButtonGroupContain
 export { default as ButtonGroup } from './elements/ButtonGroup';
 export { default as Anchor } from './views/Anchor';
 export { default as Button } from './views/Button';
+export { default as ChevronButton } from './views/icon-button/ChevronButton';
 export { default as IconButton } from './views/icon-button/IconButton';
 export { default as Icon } from './views/icon-button/Icon';
 export { default as ButtonGroupView } from './views/button-group/ButtonGroupView';
