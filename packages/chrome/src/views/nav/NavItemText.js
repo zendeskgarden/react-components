8f10bfb9cc7e4489d16756e2ebc0d65a10422f1d
Jonathan Zempel <jzempel@gmail.com>
2018-08-15 13:51:37 -0700

    Add boolean `wrap` prop to `<NavItemText>` and `<SubNavItemText>`

--- packages/chrome/src/views/nav/NavItemText.js
+++ packages/chrome/src/views/nav/NavItemText.js
@@ -5,7 +5,9 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import classNames from 'classnames';
 import { retrieveTheme } from '@zendeskgarden/react-theming';
 import ChromeStyles from '@zendeskgarden/css-chrome';
 
@@ -17,10 +19,18 @@ const COMPONENT_ID = 'chrome.nav_item_text';
 const NavItemText = styled.span.attrs({
   'data-garden-id': COMPONENT_ID,
   'data-garden-version': PACKAGE_VERSION,
-  className: ChromeStyles['c-chrome__nav__item__text']
+  className: props =>
+    classNames(ChromeStyles['c-chrome__nav__item__text'], {
+      [ChromeStyles['c-chrome__nav__item__text--wrap']]: props.wrap
+    })
 })`
   ${props => retrieveTheme(COMPONENT_ID, props)};
 `;
+
+NavItemText.propTypes = {
+  /** Wrap overflow text instead of truncating long strings with an ellipsis */
+  wrap: PropTypes.bool
+};
 
 /** @component */
 export default NavItemText;
