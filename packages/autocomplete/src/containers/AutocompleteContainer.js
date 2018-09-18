2117cb3f0d1ea961b7777d57e94883d3c7595a95
Austin Green <austingreenkansas@gmail.com>
2018-09-18 09:46:23 -0700

    fix(autocomplete): remove event default prevention for all right arrow presses (#159)

--- packages/autocomplete/src/containers/AutocompleteContainer.js
+++ packages/autocomplete/src/containers/AutocompleteContainer.js
@@ -201,41 +201,42 @@ class AutocompleteContainer extends ControlledComponent {
     this.inputRef && this.inputRef.focus();
   };
 
-  INPUT_KEYDOWN_HANDLERS = {
-    [KEY_CODES.LEFT]: e => {
-      if (isRtl(this.props)) {
-        if (
-          e.target.value === '' &&
-          this.tagSelectionModel.selectedIndex === this.tagSelectionModel.numItems - 1
-        ) {
-          this.openDropdown();
-        } else {
-          this.tagSelectionModel.selectNext();
-        }
-      } else if (e.target.value === '' && this.tagSelectionModel.selectedIndex !== 0) {
+  performPreviousKeyboardNavigation = e => {
+    if (e.target.value === '') {
+      if (this.tagSelectionModel.selectedIndex !== 0) {
         this.tagSelectionModel.selectPrevious();
         e.preventDefault();
       }
+    }
+  };
+
+  performNextKeyboardNavigation = e => {
+    const { isOpen } = this.getControlledState();
+
+    if (e.target.value === '') {
+      if (this.tagSelectionModel.selectedIndex === this.tagSelectionModel.numItems - 1) {
+        this.openDropdown();
+        e.preventDefault();
+      } else if (!isOpen) {
+        this.tagSelectionModel.selectNext();
+        e.preventDefault();
+      }
+    }
+  };
+
+  INPUT_KEYDOWN_HANDLERS = {
+    [KEY_CODES.LEFT]: e => {
+      if (isRtl(this.props)) {
+        this.performNextKeyboardNavigation(e);
+      } else {
+        this.performPreviousKeyboardNavigation(e);
+      }
     },
     [KEY_CODES.RIGHT]: e => {
-      const { isOpen } = this.getControlledState();
-
       if (isRtl(this.props)) {
-        if (e.target.value === '' && this.tagSelectionModel.selectedIndex !== 0) {
-          this.tagSelectionModel.selectPrevious();
-          e.preventDefault();
-        }
+        this.performPreviousKeyboardNavigation(e);
       } else {
-        if (
-          e.target.value === '' &&
-          this.tagSelectionModel.selectedIndex === this.tagSelectionModel.numItems - 1
-        ) {
-          this.openDropdown();
-        } else if (!isOpen) {
-          this.tagSelectionModel.selectNext();
-        }
-
-        e.preventDefault();
+        this.performNextKeyboardNavigation(e);
       }
     },
     [KEY_CODES.HOME]: e => {
