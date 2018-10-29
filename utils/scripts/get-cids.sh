7e57c47e4ea2ea4a3bda0e0c0251f86992b71e8f
Ryan Seddon <seddon.ryan@gmail.com>
2018-10-30 10:26:13 +1100

    feat(docs): Improve theming docs (#195)

---
+++ utils/scripts/get-cids.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+AWKCMD='
+  # Split record by (=)
+  BEGIN { FS = "="; }
+  # Remove all semicolons
+  { gsub(/;/, "") }
+  {
+    # On first record
+    if ( NR == 1) {
+      # Remove single quotes
+      gsub(/\047/, "");
+    } else {
+      # Replace first single quote with (,)
+      sub(/\047/, ",");
+      # Remove single quotes
+      gsub(/\047/, "");
+    }
+  }
+  # Print second column only which contains component_id
+  { print $2 }
+'
+
+grep \
+  --exclude-dir=.template \
+  --exclude-dir=node_modules \
+  --include=\*.js \
+  --exclude=\*.spec.js \
+  -rnw '../../packages' \
+  -e 'const COMPONENT_ID = ' | # Find all COMPONENT_IDs
+  sort | # Sort alphabetically
+  awk "$AWKCMD" | # Run the above awk program
+  tr '\n' ' ' # Collapse result to single line
