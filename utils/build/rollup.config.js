8502eb5cfebe85d2802dd5bd511a2220a0901007
kli <kli@zendesk.com>
2019-02-27 11:45:15 +0800

    docs: add comments regarding rollup config

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -63,41 +63,50 @@ module.exports = {
       })
   ].filter(Boolean),
   external:
+    // prevents bundling of external files, similar to webpack-node-externals library
     IS_WEB &&
     external({
       modulesFromFile: true,
       whitelist: [/@zendeskgarden\/css/u, /\.css$/u]
     }),
   plugins: [
+    // resolves node dependencies, including css and other file types, similar to webpack's resolve
     IS_WEB &&
       resolve({
         browser: true,
         preferBuiltins: false,
         extensions: ['.js', '.mjs', '.css', '.jsx']
       }),
+    // since rollup only understands ES6 exports, this allows resolving of commonjs files
     commonjs({
       include: /node_modules/u
     }),
+    // Replaces global variables with inline variables, similar to webpack's DefinePlugin
     replace({
       PACKAGE_VERSION: JSON.stringify(packageManifest.version)
     }),
+    // postcss to handle css modules and autoprefixing
     IS_WEB &&
       postcss({
+        // outputs to single styles.css file
         extract: path.join(OUTPUT_PATH, 'styles.css'),
         modules: {
           generateScopedName: '[name]__[local]___[hash:base64:5]'
         }
       }),
+    // handles loading svg as react components, similar to svgr-loader
     IS_WEB &&
       svgr({
         config: {
           plugins: { removeViewBox: false }
         }
       }),
+    // handles transpiling ES6 to ES5
     babel({
       configFile: path.resolve(__dirname, '../../babel.config.js'),
       include: /packages\/[\w-]+\/(?!node_modules)/u
     }),
+    // minifies files using terser, similar to uglifyjs
     IS_WEB && terser()
   ].filter(Boolean)
 };
