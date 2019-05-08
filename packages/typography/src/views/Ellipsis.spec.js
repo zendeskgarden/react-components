49886ec7912886783db06fe41eeadc40a8d43574
Austin Green <agreen@zendesk.com>
2019-05-08 10:10:42 -0700

    Upgrade avatars, theming, and typography packages (#327)

--- packages/typography/src/views/Ellipsis.spec.js
+++ packages/typography/src/views/Ellipsis.spec.js
@@ -6,29 +6,27 @@
  */
 
 import React from 'react';
-import { mountWithTheme } from '@zendeskgarden/react-testing';
+import { render, renderRtl } from 'garden-test-utils';
 import Ellipsis from './Ellipsis';
 
 const Example = props => <Ellipsis {...props}>Hello world</Ellipsis>;
 
-const findElement = wrapper => wrapper.find('div');
-
 describe('Ellipsis', () => {
   it('applies title by default', () => {
-    const wrapper = mountWithTheme(<Example />);
+    const { container } = render(<Example />);
 
-    expect(findElement(wrapper)).toHaveProp('title', 'Hello world');
+    expect(container.firstChild).toHaveAttribute('title', 'Hello world');
   });
 
   it('overrides title attribute if provided', () => {
-    const wrapper = mountWithTheme(<Example title="Custom title" />);
+    const { container } = render(<Example title="Custom title" />);
 
-    expect(findElement(wrapper)).toHaveProp('title', 'Custom title');
+    expect(container.firstChild).toHaveAttribute('title', 'Custom title');
   });
 
   it('applies correct styling with RTL locale', () => {
-    const wrapper = mountWithTheme(<Example />, { rtl: true });
+    const { container } = renderRtl(<Example />);
 
-    expect(wrapper).toHaveStyleRule('direction', 'rtl');
+    expect(container.firstChild).toHaveStyleRule('direction', 'rtl');
   });
 });
