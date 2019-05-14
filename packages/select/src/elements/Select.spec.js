f7e1670d1483e4ffb30ab91a1d21ea232edcc286
Austin Green <agreen@zendesk.com>
2019-05-14 13:40:47 -0700

    Upgrade dropdowns

--- packages/select/src/elements/Select.spec.js
+++ packages/select/src/elements/Select.spec.js
@@ -6,7 +6,7 @@
  */
 
 import React from 'react';
-import { mountWithTheme } from '@zendeskgarden/react-testing';
+import { render, fireEvent } from 'garden-test-utils';
 import { KEY_CODES } from '@zendeskgarden/react-selection';
 import Select from './Select';
 import Item from '../views/items/Item';
@@ -15,14 +15,13 @@ import PreviousItem from '../views/items/PreviousItem';
 import HeaderItem from '../views/items/header/HeaderItem';
 
 describe('Select', () => {
-  let wrapper;
   let onChangeSpy;
   const P_KEY_CODE = 80;
   const O_KEY_CODE = 79;
 
-  const basicExample = ({ onChange } = {}) => (
+  const BasicExample = props => (
     <Select
-      onChange={onChange}
+      {...props}
       options={[
         <HeaderItem key="header" data-test-id="header">
           Non-selectable header
@@ -48,26 +47,27 @@ describe('Select', () => {
     </Select>
   );
 
-  const findSelect = enzymeWrapper => enzymeWrapper.find('[data-test-id="select"]');
-  const findItems = enzymeWrapper => enzymeWrapper.find('[data-test-id="item"]');
-  const findDisabledItems = enzymeWrapper => enzymeWrapper.find('[data-test-id="disabled"]');
-
   beforeEach(() => {
     onChangeSpy = jest.fn();
     jest.useFakeTimers();
-    wrapper = mountWithTheme(basicExample({ onChange: onChangeSpy }));
   });
 
   describe('SelectView', () => {
     describe('onKeyDown', () => {
       it('selects first item with matching first character', () => {
-        findSelect(wrapper).simulate('keydown', { keyCode: P_KEY_CODE, key: 'p' });
+        const { getByTestId } = render(<BasicExample onChange={onChangeSpy} />);
+
+        fireEvent.keyDown(getByTestId('select'), { keyCode: P_KEY_CODE, key: 'p' });
+
         expect(onChangeSpy).toHaveBeenCalledWith('previous-item');
       });
 
       it('iterates through items with matching first character', () => {
-        findSelect(wrapper).simulate('keydown', { keyCode: O_KEY_CODE, key: 'o' });
-        findSelect(wrapper).simulate('keydown', { keyCode: O_KEY_CODE, key: 'o' });
+        const { getByTestId } = render(<BasicExample onChange={onChangeSpy} />);
+
+        fireEvent.keyDown(getByTestId('select'), { keyCode: O_KEY_CODE, key: 'o' });
+        fireEvent.keyDown(getByTestId('select'), { keyCode: O_KEY_CODE, key: 'o' });
+
         expect(onChangeSpy).toHaveBeenCalledTimes(2);
         expect(onChangeSpy).toHaveBeenLastCalledWith('2-item');
       });
@@ -76,74 +76,84 @@ describe('Select', () => {
 
   describe('Standard Items', () => {
     it('onChange is called if item is selected', () => {
-      findSelect(wrapper).simulate('click');
+      const { getByTestId, getAllByTestId } = render(<BasicExample onChange={onChangeSpy} />);
+
+      fireEvent.click(getByTestId('select'));
       jest.runOnlyPendingTimers();
-      wrapper.update();
-      findItems(wrapper)
-        .at(0)
-        .simulate('click');
+      fireEvent.click(getAllByTestId('item')[0]);
 
       expect(onChangeSpy).toHaveBeenCalledWith('1-item');
     });
 
     it('onChange is not called if item is disabled', () => {
-      findSelect(wrapper).simulate('click');
+      const { getByTestId } = render(<BasicExample onChange={onChangeSpy} />);
+
+      fireEvent.click(getByTestId('select'));
       jest.runOnlyPendingTimers();
-      wrapper.update();
-      findDisabledItems(wrapper)
-        .at(0)
-        .simulate('click');
+      fireEvent.click(getByTestId('disabled'));
 
       expect(onChangeSpy).not.toHaveBeenCalled();
     });
 
     it('retrieves textValue if children is a string', () => {
-      findSelect(wrapper).simulate('click');
-      jest.runOnlyPendingTimers();
-      wrapper.update();
-      const menuElement = findItems(wrapper).at(0);
+      const { getByTestId, getAllByTestId } = render(<BasicExample onChange={onChangeSpy} />);
 
-      menuElement.simulate('keydown', { keyCode: P_KEY_CODE, key: 'p' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.ENTER, key: 'enter' });
+      fireEvent.click(getByTestId('select'));
+      jest.runOnlyPendingTimers();
+
+      const menuElement = getAllByTestId('item')[0].parentElement;
+
+      fireEvent.keyDown(menuElement, { keyCode: P_KEY_CODE, key: 'p' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.ENTER, key: 'enter' });
+
       expect(onChangeSpy).toHaveBeenCalledWith('previous-item');
     });
 
     it('overrides textValue if provided', () => {
-      findSelect(wrapper).simulate('click');
-      jest.runOnlyPendingTimers();
-      wrapper.update();
-      const menuElement = findItems(wrapper).at(0);
+      const { getByTestId, getAllByTestId } = render(<BasicExample onChange={onChangeSpy} />);
 
-      menuElement.simulate('keydown', { keyCode: O_KEY_CODE, key: 'o' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.ENTER, key: 'enter' });
+      fireEvent.click(getByTestId('select'));
+      jest.runOnlyPendingTimers();
+
+      const menuElement = getAllByTestId('item')[0].parentElement;
+
+      fireEvent.keyDown(menuElement, { keyCode: O_KEY_CODE, key: 'o' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.ENTER, key: 'enter' });
+
       expect(onChangeSpy).toHaveBeenCalledWith('1-item');
     });
   });
 
   describe('NextItem', () => {
     it('is selected with right arrow key', () => {
-      findSelect(wrapper).simulate('click');
-      jest.runOnlyPendingTimers();
-      wrapper.update();
-      const menuElement = findItems(wrapper).at(0);
+      const { getByTestId, getAllByTestId } = render(<BasicExample onChange={onChangeSpy} />);
 
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.DOWN, key: 'down' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.DOWN, key: 'down' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.DOWN, key: 'down' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.DOWN, key: 'down' });
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.RIGHT, key: 'right' });
+      fireEvent.click(getByTestId('select'));
+      jest.runOnlyPendingTimers();
+
+      const menuElement = getAllByTestId('item')[0].parentElement;
+
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.DOWN, key: 'down' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.DOWN, key: 'down' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.DOWN, key: 'down' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.DOWN, key: 'down' });
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.RIGHT, key: 'right' });
+
       expect(onChangeSpy).toHaveBeenCalledWith('next-item');
     });
   });
 
   describe('PreviousItem', () => {
     it('is selected with left arrow key', () => {
-      findSelect(wrapper).simulate('click');
-      jest.runOnlyPendingTimers();
-      wrapper.update();
-      const menuElement = findItems(wrapper).at(0);
+      const { getByTestId, getAllByTestId } = render(<BasicExample onChange={onChangeSpy} />);
 
-      menuElement.simulate('keydown', { keyCode: KEY_CODES.LEFT, key: 'left' });
+      fireEvent.click(getByTestId('select'));
+      jest.runOnlyPendingTimers();
+
+      const menuElement = getAllByTestId('item')[0].parentElement;
+
+      fireEvent.keyDown(menuElement, { keyCode: KEY_CODES.LEFT, key: 'left' });
+
       expect(onChangeSpy).toHaveBeenCalledWith('previous-item');
     });
   });
