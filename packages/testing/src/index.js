fd3dced6256976bdbce43b5a743e7701d1aa78b5
Austin Green <agreen@zendesk.com>
2019-05-15 10:44:07 -0700

    Remove enzyme areas of react-testing

--- packages/testing/src/index.js
+++ packages/testing/src/index.js
@@ -5,7 +5,4 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-export { default as mountWithTheme } from './utils/mountWithTheme.js';
-export { default as renderWithTheme } from './utils/renderWithTheme.js';
-export { default as shallowWithTheme } from './utils/shallowWithTheme.js';
 export { default as getExports } from './utils/getExports.js';
