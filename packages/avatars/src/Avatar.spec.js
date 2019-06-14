0c0ea2486d069ce26cf5a6d88362b301bff5bc39
Austin Green <agreen@zendesk.com>
2019-06-14 09:48:27 -0700

    fix(avatars): update propTypes to accurately check badge type (#367)

--- packages/avatars/src/Avatar.spec.js
+++ packages/avatars/src/Avatar.spec.js
@@ -46,14 +46,6 @@ describe('Avatar', () => {
     expect(container.firstChild).toHaveAttribute('data-badge');
   });
 
-  it('does not render badge if away status is provided', () => {
-    const { queryByTestId } = render(
-      <Avatar status="away" badge={<span data-test-id="badge">2</span>} />
-    );
-
-    expect(queryByTestId('badge')).toBe(null);
-  });
-
   it('renders text element if provided', () => {
     const { getByTestId } = render(
       <Avatar>
