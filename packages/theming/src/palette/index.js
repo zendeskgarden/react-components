5ba083a5a26635907dad5a5d355f9b9ef9463193
Jonathan Zempel <jzempel@gmail.com>
2019-07-17 13:30:07 -0700

    Module consolidation

--- packages/theming/src/palette/index.js
+++ packages/theming/src/palette/index.js
@@ -5,50 +5,150 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import { default as azure } from './azure';
-import { default as blue } from './blue';
-import { default as crimson } from './crimson';
-import { default as fuschia } from './fuschia';
-import { default as green } from './green';
-import { default as grey } from './grey';
-import { default as kale } from './kale';
-import { default as lemon } from './lemon';
-import { default as lime } from './lime';
-import { default as mint } from './mint';
-import { default as orange } from './orange';
-import { default as pink } from './pink';
-import { default as purple } from './purple';
-import { default as product } from './product';
-import { default as red } from './red';
-import { default as royal } from './royal';
-import { default as teal } from './teal';
-import { default as yellow } from './yellow';
-
+/** @ignore */
 const PALETTE = {
   black: '#000',
   white: '#fff',
-  product,
+  product: {
+    support: '#78a300',
+    message: '#37b8af',
+    explore: '#30aabc',
+    guide: '#eb4962',
+    connect: '#eb6651',
+    chat: '#f79a3e',
+    talk: '#efc93d',
+    sell: '#d4ae5e'
+  },
 
   /* primary */
-  grey,
-  blue,
-  red,
-  yellow,
-  green,
-  kale,
+  grey: {
+    100: '#f8f9f9',
+    200: '#e9ebed',
+    300: '#d8dcde',
+    400: '#c2c8cc',
+    500: '#87929d',
+    600: '#68737d',
+    700: '#49545c',
+    800: '#2f3941'
+  },
+  blue: {
+    100: '#edf7ff',
+    200: '#cee2f2',
+    300: '#adcce4',
+    400: '#5293c7',
+    500: '#337fbd',
+    600: '#1f73b7',
+    700: '#144a75',
+    800: '#0f3554'
+  },
+  red: {
+    100: '#fff0f1',
+    200: '#f5d5d8',
+    300: '#f5b5ba',
+    400: '#e35b66',
+    500: '#d93f4c',
+    600: '#cc3340',
+    700: '#8c232c',
+    800: '#681219'
+  },
+  yellow: {
+    100: '#fff8ed',
+    200: '#fff0db',
+    300: '#fcdba9',
+    400: '#ffb648',
+    500: '#f5a133',
+    600: '#ed961c',
+    700: '#ad5e18',
+    800: '#703b15'
+  },
+  green: {
+    100: '#edf8f4',
+    200: '#d1e8df',
+    300: '#aecfc2',
+    400: '#5eae91',
+    500: '#228f67',
+    600: '#038153',
+    700: '#186146',
+    800: '#0b3b29'
+  },
+  kale: {
+    100: '#f5fbfc',
+    200: '#c1d6d9',
+    300: '#819a9e',
+    400: '#56777a',
+    500: '#335d63',
+    600: '#04444d',
+    700: '#03363d',
+    800: '#012b30'
+  },
 
   /* secondary */
-  fuschia,
-  pink,
-  crimson,
-  orange,
-  lemon,
-  lime,
-  mint,
-  teal,
-  azure,
-  royal,
-  purple
+  fuschia: {
+    400: '#d653c2',
+    600: '#a81897',
+    M400: '#cf62a8',
+    M600: '#a8458c'
+  },
+  pink: {
+    400: '#ec4d63',
+    600: '#d42054',
+    M400: '#d57287',
+    M600: '#b23a5d'
+  },
+  crimson: {
+    400: '#e34f32',
+    600: '#c72a1c',
+    M400: '#cc6c5b',
+    M600: '#b24a3c'
+  },
+  orange: {
+    400: '#de701d',
+    600: '#bf5000',
+    M400: '#d4772c',
+    M600: '#b35827'
+  },
+  lemon: {
+    400: '#ffd424',
+    600: '#ffbb10',
+    M400: '#e7a500',
+    M600: '#c38f00'
+  },
+  lime: {
+    400: '#43b324',
+    600: '#2e8200',
+    M400: '#519e2d',
+    M600: '#47782c'
+  },
+  mint: {
+    400: '#00a656',
+    600: '#058541',
+    M400: '#299c66',
+    M600: '#2e8057'
+  },
+  teal: {
+    400: '#02a191',
+    600: '#028079',
+    M400: '#2d9e8f',
+    M600: '#3c7873'
+  },
+  azure: {
+    400: '#3091ec',
+    600: '#1371d6',
+    M400: '#5f8dcf',
+    M600: '#3a70b2'
+  },
+  royal: {
+    400: '#5d7df5',
+    600: '#3353e2',
+    M400: '#7986d8',
+    M600: '#4b61c3'
+  },
+  purple: {
+    400: '#b552e2',
+    600: '#6a27b8',
+    M400: '#b072cc',
+    M600: '#9358b0'
+  }
 };
 
 /** @component */
--- packages/theming/src/theme/index.js
+++ packages/theming/src/theme/index.js
@@ -5,18 +5,129 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import { borders, borderRadii, borderStyles, borderWidths } from './borders';
-import { default as colors } from './colors';
-import { fonts, fontSizes, fontWeights } from './fonts';
-import { default as lineHeights } from './lineHeights';
-import { shadows, shadowWidths } from './shadows';
-import { default as space } from './space';
+import { math, rgba } from 'polished';
 import PALETTE from '../palette';
+import getColor from '../utils/getColor';
+
+const BASE = 4;
+
+const borderRadii = {
+  sm: `${BASE / 2}px`,
+  md: `${BASE}px`
+};
+
+const borderStyles = {
+  solid: 'solid'
+};
+
+const borderWidths = {
+  sm: '1px',
+  md: '3px'
+};
+
+const borders = {
+  sm: `${borderWidths.sm} ${borderStyles.solid}`,
+  md: `${borderWidths.md} ${borderStyles.solid}`
+};
+
+const colors = {
+  base: 'light',
+  background: PALETTE.white,
+  foreground: PALETTE.grey[800],
+  primaryHue: 'blue',
+  dangerHue: 'red',
+  warningHue: 'yellow',
+  successHue: 'green',
+  neutralHue: 'grey',
+  chromeHue: 'kale'
+};
+
+const fonts = {
+  mono: [
+    'SFMono-Regular' /* macOS */,
+    'Consolas' /* Windows */,
+    '"Liberation Mono"' /* Ubuntu */,
+    'Menlo',
+    'Courier',
+    'monospace'
+  ].join(','),
+  system: [
+    'system-ui' /* drafts.csswg.org/css-fonts-4/#system-ui */,
+    '-apple-system' /* macOS Safari & FF (San Francisco) */,
+    'BlinkMacSystemFont' /* macOS Chrome (San Francisco) */,
+    '"Segoe UI"' /* Windows */,
+    'Roboto' /* Android & ChromeOS */,
+    'Oxygen-Sans' /* KDE */,
+    'Ubuntu' /* Ubuntu */,
+    'Cantarell' /* GNOME */,
+    '"Helvetica Neue"',
+    'Arial',
+    'sans-serif'
+  ].join(',')
+};
+
+const fontSizes = {
+  sm: '12px',
+  md: '14px',
+  lg: '18px',
+  xl: '22px',
+  xxl: '26px',
+  xxxl: '36px'
+};
+
+fontSizes.mono = {
+  sm: math(`${fontSizes.sm} - 1px`),
+  md: math(`${fontSizes.md} - 1px`),
+  lg: math(`${fontSizes.lg} - 1px`)
+};
+
+const fontWeights = {
+  thin: 100,
+  extralight: 200,
+  light: 300,
+  regular: 400,
+  semibold: 500,
+  bold: 600,
+  extrabold: 700,
+  black: 800
+};
+
+const lineHeights = {
+  sm: `${BASE * 4}px`,
+  md: `${BASE * 5}px`,
+  lg: `${BASE * 6}px`,
+  xl: `${BASE * 7}px`,
+  xxl: `${BASE * 8}px`,
+  xxxl: `${BASE * 11}px`
+};
 
 const palette = { ...PALETTE };
 
 /* Exclude product palette from the theme */
 delete palette.product;
+
+const shadowWidths = {
+  sm: '2px',
+  md: '3px'
+};
+
+const shadows = {
+  sm: color => `0 0 0 ${shadowWidths.sm} ${color}`,
+  md: color => `0 0 0 ${shadowWidths.md} ${color}`,
+  lg: (offsetY, blurRadius, theme) =>
+    `0 ${offsetY} ${blurRadius} 0 ${rgba(getColor('chromeHue', 600, theme), 0.15)}`
+};
+
+const space = {
+  base: BASE,
+  xxs: `${BASE}px`,
+  xs: `${BASE * 2}px`,
+  sm: `${BASE * 3}px`,
+  md: `${BASE * 5}px`,
+  lg: `${BASE * 8}px`,
+  xl: `${BASE * 10}px`,
+  xxl: `${BASE * 12}px`
+};
 
 const DEFAULT_THEME = {
   borders,
