afa0d99e225f97ee4a57bb51f036f80770bf4f97
Jonathan Zempel <jzempel@gmail.com>
2019-09-24 16:43:01 -0700

    Root TS configuration

--- packages/avatars/styleguide.config.js
+++ packages/avatars/styleguide.config.js
@@ -1,9 +1,29 @@
 /**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+const path = require('path');
+const reactDocgenTypescript = require('react-docgen-typescript');
+const reactDocgen = require('react-docgen');
+
+/**
  * Package specific styleguide configuration
  * https://github.com/styleguidist/react-styleguidist/blob/master/docs/Configuration.md
  */
 
 module.exports = {
+  propsParser: reactDocgenTypescript.withCustomConfig(
+    path.resolve(__dirname, '../../tsconfig.json'),
+    {
+      propFilter: props => {
+        return props.parent.fileName.indexOf('node_modules') === -1;
+      }
+    }
+  ).parse,
+  resolver: reactDocgen.resolver.findAllComponentDefinitions,
   sections: [
     {
       name: '',
@@ -24,7 +44,7 @@ module.exports = {
     },
     {
       name: 'Components',
-      components: '../../packages/avatars/src/components/[A-Z]*.js'
+      components: '../../packages/avatars/src/components/Avatar.tsx'
     }
   ]
 };
