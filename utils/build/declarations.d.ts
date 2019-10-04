e0ea337809168824f04d4f232a0940a3b8e0d58b
Austin Green <agreen@zendesk.com>
2019-10-04 10:57:59 -0700

    chore(selection): remove all react-selection KEY_CODES usage

--- utils/build/declarations.d.ts
+++ utils/build/declarations.d.ts
@@ -10,38 +10,6 @@ declare const PACKAGE_VERSION: string;
 
 /** React packages */
 declare module '@zendeskgarden/container-selection';
-declare module '@zendeskgarden/container-utilities' {
-  interface IKeyCodes {
-    ALT: 18;
-    BACKSPACE: 8;
-    COMMA: 188;
-    DELETE: 46;
-    DOWN: 40;
-    END: 35;
-    ENTER: 13;
-    ESCAPE: 27;
-    HOME: 36;
-    LEFT: 37;
-    NUMPAD_ADD: 107;
-    NUMPAD_DECIMAL: 110;
-    NUMPAD_DIVIDE: 111;
-    NUMPAD_ENTER: 108;
-    NUMPAD_MULTIPLY: 106;
-    NUMPAD_SUBTRACT: 109;
-    PAGE_DOWN: 34;
-    PAGE_UP: 33;
-    PERIOD: 190;
-    RIGHT: 39;
-    SHIFT: 16;
-    SPACE: 32;
-    TAB: 9;
-    UP: 38;
-  }
-
-  export const KEY_CODES: IKeyCodes;
-
-  export const composeEventHandlers: any;
-}
 declare module '@zendeskgarden/react-theming';
 declare module '@zendeskgarden/react-buttons';
 declare module '@zendeskgarden/react-grid';
