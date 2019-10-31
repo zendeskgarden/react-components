1c52307e6250df200f9d95e4b76ecf8967ea00c4
Austin Green <agreen@zendesk.com>
2019-10-31 09:56:51 -0700

    Fix bundlesize errors

--- bundlesize.config.js
+++ bundlesize.config.js
@@ -5,24 +5,30 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-const { readdirSync, readFileSync } = require('fs');
+const { readdirSync, readFileSync, existsSync } = require('fs');
 const path = require('path');
 
 const packageFolders = readdirSync(path.resolve(__dirname, 'packages'));
 const validPackageNames = packageFolders.filter(name => !name.startsWith('.'));
 
-const files = validPackageNames.map(packageName => {
-  const packageJsonText = readFileSync(
-    path.resolve(__dirname, 'packages', packageName, 'package.json')
-  );
+const files = [];
+
+for (const packageName of validPackageNames) {
+  const PACKAGE_JSON_PATH = path.resolve(__dirname, 'packages', packageName, 'package.json');
+
+  if (!existsSync(PACKAGE_JSON_PATH)) {
+    continue;
+  }
+
+  const packageJsonText = readFileSync(PACKAGE_JSON_PATH);
   const packageJson = JSON.parse(packageJsonText);
   const maxSize = packageJson['zendeskgarden:max_size'];
 
-  return {
+  files.push({
     path: path.resolve(__dirname, 'packages', packageName, 'dist', 'umd', 'bundle.min.js'),
     maxSize: maxSize || '1 B'
-  };
-});
+  });
+}
 
 module.exports = {
   files
