1c0e2d4cbbcba0f21cef4dbc4665dd6bd0d3ee3b
Jonathan Zempel <jzempel@gmail.com>
2019-11-05 15:27:33 -0800

    Prefer useCombinedRefs per comments from @austin94

--- packages/forms/src/elements/Checkbox.js
+++ packages/forms/src/elements/Checkbox.js
@@ -5,8 +5,9 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { useCombinedRefs } from '@zendeskgarden/container-utilities';
 import useFieldContext from '../utils/useFieldContext';
 import { CheckboxContext } from '../utils/useCheckboxContext';
 import { StyledCheckInput } from '../styled';
@@ -17,26 +18,17 @@ import { StyledCheckInput } from '../styled';
  */
 const Checkbox = React.forwardRef(({ indeterminate, children, ...props }, ref) => {
   const { getInputProps } = useFieldContext();
-  const inputRef = inputElement => {
-    inputElement && (inputElement.indeterminate = indeterminate);
-  };
-  const combinedRef = inputElement => {
-    [inputRef, ref].forEach(targetRef => {
-      if (targetRef) {
-        if (typeof targetRef === 'function') {
-          targetRef(inputElement);
-        } else {
-          targetRef.current = inputElement;
-        }
-      }
-    });
-  };
+  const inputRef = useCombinedRefs(ref);
+
+  useEffect(() => {
+    inputRef.current.indeterminate = indeterminate;
+  }, [indeterminate, inputRef]);
 
   return (
     <CheckboxContext.Provider value={{}}>
       <StyledCheckInput
         {...getInputProps({
-          ref: combinedRef,
+          ref: inputRef,
           ...props
         })}
       />
