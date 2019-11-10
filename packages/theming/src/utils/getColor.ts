1dcf48f90c9017eaaa98efbf7aa03d17bb47758b
Jonathan Zempel <jzempel@gmail.com>
2019-11-10 15:43:39 -0800

    Move hoc to elements

--- packages/theming/src/utils/getColor.ts
+++ packages/theming/src/utils/getColor.ts
@@ -7,7 +7,7 @@
 
 /* eslint-disable default-param-last */
 
-import DEFAULT_THEME from '../theme';
+import DEFAULT_THEME from '../elements/theme';
 import darken from 'polished/lib/color/darken';
 import lighten from 'polished/lib/color/lighten';
 import rgba from 'polished/lib/color/rgba';
