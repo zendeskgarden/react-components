9e912511c2eec5469575e372e61eb08b22a24805
Jonathan Zempel <jzempel@gmail.com>
2019-12-23 15:06:08 -0800

    Add getLineHeight utility

---
+++ packages/theming/src/utils/getLineHeight.spec.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import getLineHeight from './getLineHeight';
+
+describe('getLineHeight', () => {
+  it('gets line height for pixel values', () => {
+    const lineHeight = getLineHeight('20px', '14px');
+    const expected = 20 / 14;
+
+    expect(lineHeight).toBe(expected);
+  });
+
+  it('gets line height for integer values', () => {
+    const lineHeight = getLineHeight(20, 14);
+    const expected = 20 / 14;
+
+    expect(lineHeight).toBe(expected);
+  });
+
+  it('gets line height for mixed values', () => {
+    const lineHeight = getLineHeight('20px', 14);
+    const expected = 20 / 14;
+
+    expect(lineHeight).toBe(expected);
+  });
+
+  it('throws if called with non-pixel values', () => {
+    /* eslint-disable no-console */
+    const originalError = console.error;
+
+    console.error = jest.fn();
+
+    expect(() => getLineHeight('2em', 14)).toThrow();
+
+    console.error = originalError;
+  });
+});
---
+++ packages/theming/src/utils/getLineHeight.ts
@@ -0,0 +1,31 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import stripUnit from 'polished/lib/helpers/stripUnit';
+
+/**
+ * Get unitless line height based on the given pixel-valued height and font size.
+ *
+ * @param {string|number} height Desired line height in pixels.
+ * @param {string|number} fontSize Font size (in pixels) of text contained within the line.
+ *
+ * @component
+ */
+export default function getLineHeight(height: string | number, fontSize: string | number) {
+  const [heightValue, heightUnit] = stripUnit(height, true /* unitReturn */);
+  const [fontSizeValue, fontSizeUnit] = stripUnit(fontSize, true /* unitReturn */);
+
+  if (heightUnit && heightUnit !== 'px') {
+    throw new Error(`Unexpected \`height\` with '${heightUnit}' units.`);
+  }
+
+  if (fontSizeUnit && fontSizeUnit !== 'px') {
+    throw new Error(`Unexpected \`fontSize\` with '${fontSizeUnit}' units.`);
+  }
+
+  return heightValue / fontSizeValue;
+}
