c0c4bd2a4c1ac75d6d3eb7220f9fafefd121168b
Jonathan Zempel <jzempel@gmail.com>
2020-03-11 17:02:52 -0700

    Test deploy

--- utils/scripts/gh-pages.js
+++ utils/scripts/gh-pages.js
@@ -10,6 +10,5 @@
 const ghPages = require('gh-pages');
 
 ghPages.publish('demo', {
-  repo: `https://${process.env.GITHUB_TOKEN}@github.com/zendeskgarden/react-components.git`,
-  silent: true
+  repo: `https://${process.env.GITHUB_TOKEN}@github.com/zendeskgarden/react-components.git`
 });
