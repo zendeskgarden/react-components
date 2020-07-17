4906e9acd74e793e9dd0498e9c6ad9157d9536ec
Jonathan Zempel <jzempel@gmail.com>
2020-07-17 09:15:15 -0700

    Add mediaQuery unit testing

--- packages/theming/src/utils/mediaQuery.ts
+++ packages/theming/src/utils/mediaQuery.ts
@@ -7,33 +7,45 @@
 
 import DEFAULT_THEME from '../elements/theme';
 import { DefaultTheme } from 'styled-components';
-import { math } from 'polished';
+import { getValueAndUnit } from 'polished';
 
 type QUERY = 'up' | 'down' | 'only' | 'between';
-type KEY = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
-type BREAKPOINT = KEY | [KEY, KEY];
+type BREAKPOINT = 'xs' | 'sm' | 'md' | 'lg' | 'xl';
 
-const maxWidth = (breakpoints: DefaultTheme['breakpoints'], key: KEY) => {
+const maxWidth = (breakpoints: DefaultTheme['breakpoints'], breakpoint: BREAKPOINT) => {
   const keys = Object.keys(breakpoints);
-  const index = keys.indexOf(key) + 1;
-  const value = keys[index];
+  const index = keys.indexOf(breakpoint) + 1;
 
-  return value && math(`${value} - .02`);
+  if (keys[index]) {
+    const dimension = getValueAndUnit(breakpoints[keys[index] as BREAKPOINT]);
+    const value = dimension[0] - 0.02;
+    const unit = dimension[1];
+
+    return `${value}${unit}`;
+  }
+
+  // When the index advances past the end of the available breakpoints.
+  return undefined;
 };
 
 /**
- * Get a media query string for the given function specifier, breakpoint name, and theme.
+ * Get a media query string for the given query specifier, breakpoint name, and theme.
  *
  * @param {string} query A query specifier, one of:
- * - `'up'` = match screen widths greater than and including the given breakpoint.
- * - `'down'` = match screen widths less than and included within the given breakpoint.
+ * - `'up'` = match screen widths including the given breakpoint and up.
+ * - `'down'` = match screen widths included within the given breakpoint and down.
  * - `'only'` = match screen widths included within the given breakpoint.
- * - `'between'` = match screen widths greater than and including the first breakpoint,
- *   and less than and included within the second breakpoint.
- * @param {string} breakpoint A `theme.breakpoints` key, one of: `'xs'`, `'sm'`, `'md'`, `'lg'`, `'xl'`.
+ * - `'between'` = match screen widths including the first breakpoint up through
+ *   screen widths included within the second breakpoint.
+ * @param {string|Array} breakpoint A `theme.breakpoints` key, one of: `'xs'`, `'sm'`,
+ *   `'md'`, `'lg'`, `'xl'`; or an array of two keys when 'between' is the specified query.
  * @param {Object} theme Context `theme` object.
  */
-export default function mediaQuery(query: QUERY, breakpoint: BREAKPOINT, theme?: DefaultTheme) {
+export default function mediaQuery(
+  query: QUERY,
+  breakpoint: BREAKPOINT | [BREAKPOINT, BREAKPOINT],
+  theme?: DefaultTheme
+) {
   let retVal;
   let min;
   let max;
@@ -45,7 +57,7 @@ export default function mediaQuery(query: QUERY, breakpoint: BREAKPOINT, theme?:
     } else if (query === 'down') {
       if (breakpoint === 'xl') {
         // Down from the XL breakpoint is the same as up from zero.
-        min = 0;
+        min = DEFAULT_THEME.breakpoints.xs;
       } else {
         max = maxWidth(breakpoints, breakpoint);
       }
@@ -66,6 +78,8 @@ export default function mediaQuery(query: QUERY, breakpoint: BREAKPOINT, theme?:
     }
   } else if (max) {
     retVal = `@media (max-width: ${max})`;
+  } else {
+    throw new Error(`Unexpected query and breakpoint combination: '${query}', '${breakpoint}'.`);
   }
 
   return retVal;
