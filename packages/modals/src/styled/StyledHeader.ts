517c0d991aff7c39280c7b2ee3f0432b4f5d1d49
Austin Green <agreen@zendesk.com>
2020-08-12 13:37:53 -0700

    fix(modals): increase padding for overflown header text

--- packages/modals/src/styled/StyledHeader.ts
+++ packages/modals/src/styled/StyledHeader.ts
@@ -5,7 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import styled from 'styled-components';
+import styled, { css, ThemeProps, DefaultTheme } from 'styled-components';
 import {
   getLineHeight,
   getColor,
@@ -22,20 +22,35 @@ export interface IStyledHeaderProps {
   isDanger?: boolean;
 }
 
+const sizeStyles = (props: ThemeProps<DefaultTheme>) => {
+  const verticalPadding = `${props.theme.space.base * 5}px`;
+  const startPadding = `${props.theme.space.base * 10}px`;
+  const endPadding = `${props.theme.space.base * 15}px`;
+
+  return css`
+    margin: 0;
+    padding-top: ${verticalPadding};
+    padding-right: ${props.theme.rtl ? startPadding : endPadding};
+    padding-bottom: ${verticalPadding};
+    /* stylelint-disable-next-line */
+    padding-left: ${props.theme.rtl ? endPadding : startPadding};
+  `;
+};
+
 export const StyledHeader = styled.div.attrs<IStyledHeaderProps>({
   'data-garden-id': COMPONENT_ID,
   'data-garden-version': PACKAGE_VERSION
 })<IStyledHeaderProps>`
   display: block;
   position: ${props => props.isDanger && 'relative'};
-  margin: 0;
   border-bottom: ${props => props.theme.borders.sm} ${getColor('neutralHue', 200)};
-  padding: ${props => `${props.theme.space.base * 5}px ${props.theme.space.base * 10}px`};
   line-height: ${props => getLineHeight(props.theme.lineHeights.md, props.theme.fontSizes.md)};
   color: ${props =>
     props.isDanger ? getColor('dangerHue', 600, props.theme) : props.theme.colors.foreground};
   font-size: ${props => props.theme.fontSizes.md};
   font-weight: ${props => props.theme.fontWeights.semibold};
+
+  ${props => sizeStyles(props)};
 
   ${props => retrieveComponentStyles(COMPONENT_ID, props)};
 `;
