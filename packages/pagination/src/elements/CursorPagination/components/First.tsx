2733c3049c98857a986ac0cb9d48af216da0c2da
hzhu <hz@henryzhu.me>
2020-08-31 15:20:12 -0700

    fix(pagination): fix cursor button type definitions

--- packages/pagination/src/elements/CursorPagination/components/First.tsx
+++ packages/pagination/src/elements/CursorPagination/components/First.tsx
@@ -5,11 +5,11 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { HTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import ChevronDoubleLeft from '@zendeskgarden/svg-icons/src/16/chevron-double-left-stroke.svg';
 import { StyledIcon, StyledCursor } from '../../../styled';
 
-export const First = React.forwardRef<HTMLButtonElement, HTMLAttributes<HTMLButtonElement>>(
+export const First = React.forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButtonElement>>(
   ({ children, ...other }, ref) => {
     return (
       <StyledCursor ref={ref} as="button" {...other}>
