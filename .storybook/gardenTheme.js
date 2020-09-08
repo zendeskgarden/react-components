e0dc080b0dbc217dd053c18ef2c7ee6080ef1bbc
Austin Green <agreen@zendesk.com>
2020-09-08 13:45:42 -0700

    Remove unused palette object

--- .storybook/gardenTheme.js
+++ .storybook/gardenTheme.js
@@ -6,7 +6,7 @@
  */
 
 import { create } from '@storybook/theming/create';
-import { DEFAULT_THEME, PALETTE } from '../packages/theming/src';
+import { DEFAULT_THEME } from '../packages/theming/src';
 
 export const managerTheme = create({
   base: 'light',
