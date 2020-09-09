3cee9e5c5a93ea68cae97d4eb42fc0b464cc7d27
Austin Green <agreen@zendesk.com>
2020-09-09 11:50:58 -0700

    Automatically generate babel alias

--- .storybook/babel.config.js
+++ .storybook/babel.config.js
@@ -5,7 +5,13 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+const path = require('path');
+const { readdirSync } = require('fs');
 const storybookBabelConfig = require('@storybook/core/dist/server/common/babel');
+
+const PACKAGE_NAMES = readdirSync(path.resolve(__dirname, '../packages')).filter(
+  name => name !== '.template'
+);
 
 module.exports = {
   sourceType: 'unambiguous',
@@ -17,28 +23,11 @@ module.exports = {
       'module-resolver',
       {
         root: ['../'],
-        alias: {
-          '@zendeskgarden/react-accordions': './packages/accordions/src',
-          '@zendeskgarden/react-avatars': './packages/avatars/src',
-          '@zendeskgarden/react-breadcrumbs': './packages/breadcrumbs/src',
-          '@zendeskgarden/react-buttons': './packages/buttons/src',
-          '@zendeskgarden/react-chrome': './packages/chrome/src',
-          '@zendeskgarden/react-datepickers': './packages/datepickers/src',
-          '@zendeskgarden/react-dropdowns': './packages/dropdowns/src',
-          '@zendeskgarden/react-forms': './packages/forms/src',
-          '@zendeskgarden/react-grid': './packages/grid/src',
-          '@zendeskgarden/react-loaders': './packages/loaders/src',
-          '@zendeskgarden/react-modals': './packages/modals/src',
-          '@zendeskgarden/react-notifications': './packages/notifications/src',
-          '@zendeskgarden/react-pagination': './packages/pagination/src',
-          '@zendeskgarden/react-tables': './packages/tables/src',
-          '@zendeskgarden/react-tabs': './packages/tabs/src',
-          '@zendeskgarden/react-tags': './packages/tags/src',
-          '@zendeskgarden/react-theming': './packages/theming/src',
-          '@zendeskgarden/react-tooltips': './packages/tooltips/src',
-          '@zendeskgarden/react-typography': './packages/typography/src',
-          '@zendeskgarden/react-utilities': './packages/utilities/src'
-        }
+        alias: PACKAGE_NAMES.reduce((previousValue, packageName) => {
+          previousValue[`@zendeskgarden/react-${packageName}`] = `./packages/${packageName}/src`;
+
+          return previousValue;
+        }, {})
       }
     ]
   ],
