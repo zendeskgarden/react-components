e769579c6b382e3e5ed855c938c5e3f54e54d04e
hzhu <hz@henryzhu.me>
2020-10-27 10:17:37 -0700

    Update TS exports

--- packages/pagination/src/index.ts
+++ packages/pagination/src/index.ts
@@ -5,9 +5,6 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-export {
-  default as Pagination,
-  IPaginationProps,
-  PAGE_TYPE
-} from './elements/Pagination/Pagination';
+export { default as Pagination } from './elements/Pagination/Pagination';
+export type { IPaginationProps, PAGE_TYPE } from './elements/Pagination/Pagination';
 export { CursorPagination } from './elements/CursorPagination/CursorPagination';
