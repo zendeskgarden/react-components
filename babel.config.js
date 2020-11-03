e136f1ccf0187d850505b9abea80833bb6736330
Austin Green <agreen@zendesk.com>
2020-11-03 15:45:14 -0800

    chore(deps): update non-major shared dependencies

--- babel.config.js
+++ babel.config.js
@@ -17,7 +17,12 @@ module.exports = {
   ],
   plugins: [
     '@babel/plugin-transform-object-assign',
-    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-proposal-class-properties',
+      {
+        loose: true
+      }
+    ],
     '@babel/proposal-object-rest-spread',
     'babel-plugin-styled-components'
   ],
