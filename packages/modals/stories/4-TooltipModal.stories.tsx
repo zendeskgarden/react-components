74e5dbb33e2a240b63d4dbddd3dc11ac80d74410
hzhu <hz@henryzhu.me>
2020-11-05 09:38:20 -0800

    Update TooltipModal story

--- packages/modals/stories/4-TooltipModal.stories.tsx
+++ packages/modals/stories/4-TooltipModal.stories.tsx
@@ -5,7 +5,9 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { useState, useRef } from 'react';
+import React from 'react';
+import styled from 'styled-components';
+import { getColor } from '@zendeskgarden/react-theming';
 import { Story, Meta } from '@storybook/react';
 import { Button } from '@zendeskgarden/react-buttons';
 import { TooltipModal } from '@zendeskgarden/react-modals';
@@ -16,77 +18,132 @@ export default {
   component: TooltipModal
 } as Meta;
 
+const StyledProgress = styled(TooltipModal.FooterItem)`
+  flex-grow: 1;
+  color: ${p => getColor('neutralHue', 600, p.theme)};
+  font-size: ${p => p.theme.fontSizes.sm};
+`;
+
 export const Default: Story = ({
-  placement,
-  hasArrow,
+  id,
   zIndex,
-  backdropProps,
-  focusOnMount,
+  hasArrow,
+  isAnimated,
   restoreFocus,
-  id
+  focusOnMount,
+  backdropProps
 }) => {
-  const buttonRef = useRef<HTMLButtonElement>(null);
-  const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>();
+  const [step, setStep] = React.useState<any>();
+  const step1Ref = React.useRef(null);
+  const step2Ref = React.useRef(null);
+  const step3Ref = React.useRef(null);
+  const step4Ref = React.useRef(null);
+
+  const referenceElement = React.useMemo(() => {
+    if (step === 1) {
+      return step1Ref.current;
+    } else if (step === 2) {
+      return step2Ref.current;
+    } else if (step === 3) {
+      return step3Ref.current;
+    } else if (step === 4) {
+      return step4Ref.current;
+    }
+
+    return undefined;
+  }, [step]);
+
+  const placement = React.useMemo(() => {
+    if (step === 1) {
+      return 'bottom-start';
+    } else if (step === 2 || step === 3) {
+      return 'bottom';
+    } else if (step === 4) {
+      return 'bottom-end';
+    }
+
+    return undefined;
+  }, [step]);
 
   return (
     <Grid>
-      <Row>
-        <Col textAlign="center" style={{ padding: 50 }}>
-          <Button
-            ref={buttonRef}
-            onClick={() => {
-              setReferenceElement(buttonRef.current);
-            }}
-          >
-            Tooltip modal
+      <Row justifyContent="center">
+        <Col>
+          <Button ref={step1Ref} onClick={() => setStep(1)}>
+            Step 1
           </Button>
-          <TooltipModal
-            referenceElement={referenceElement}
-            onClose={() => setReferenceElement(null)}
-            placement={placement}
-            hasArrow={hasArrow}
-            zIndex={zIndex}
-            backdropProps={backdropProps}
-            focusOnMount={focusOnMount}
-            restoreFocus={restoreFocus}
-            id={id}
-          >
-            <TooltipModal.Title>Tooltip modal header</TooltipModal.Title>
-            <TooltipModal.Body>
-              Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea
-              sprouts fava bean collard greens dandelion okra wakame tomato.
-            </TooltipModal.Body>
-            <TooltipModal.Close aria-label="Close" />
-          </TooltipModal>
+        </Col>
+        <Col>
+          <Button ref={step2Ref} onClick={() => setStep(2)}>
+            Step 2
+          </Button>
+        </Col>
+        <Col>
+          <Button ref={step3Ref} onClick={() => setStep(3)}>
+            Step 3
+          </Button>
+        </Col>
+        <Col>
+          <Button ref={step4Ref} onClick={() => setStep(4)}>
+            Step 4
+          </Button>
         </Col>
       </Row>
+
+      <TooltipModal
+        id={id}
+        zIndex={zIndex}
+        hasArrow={hasArrow}
+        placement={placement}
+        isAnimated={isAnimated}
+        restoreFocus={restoreFocus}
+        focusOnMount={focusOnMount}
+        backdropProps={backdropProps}
+        onClose={() => setStep(undefined)}
+        referenceElement={referenceElement}
+      >
+        <TooltipModal.Title>Title for step {step}</TooltipModal.Title>
+        <TooltipModal.Body>
+          Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea
+          sprouts fava bean collard greens dandelion okra wakame tomato. Dandelion cucumber earthnut
+          pea peanut soko zucchini.
+        </TooltipModal.Body>
+        <TooltipModal.Footer>
+          <StyledProgress>
+            <span>{step} of 4</span>
+          </StyledProgress>
+          <TooltipModal.FooterItem>
+            {step > 1 && (
+              <Button size="small" onClick={() => setStep(step - 1)}>
+                Previous
+              </Button>
+            )}
+          </TooltipModal.FooterItem>
+          <TooltipModal.FooterItem>
+            <Button
+              size="small"
+              isPrimary
+              onClick={() => {
+                if (step === 4) {
+                  setStep(undefined);
+                } else {
+                  setStep(step + 1);
+                }
+              }}
+            >
+              {step === 4 ? 'Finish' : 'Next'}
+            </Button>
+          </TooltipModal.FooterItem>
+        </TooltipModal.Footer>
+        <TooltipModal.Close aria-label="Close" />
+      </TooltipModal>
     </Grid>
   );
 };
 
 Default.argTypes = {
-  referenceElement: { control: { disable: true } },
-  popperModifiers: { control: { disable: true } },
-  backdropProps: { control: { disable: true } },
-  placement: {
-    control: {
-      type: 'select',
-      options: [
-        'auto',
-        'top',
-        'top-start',
-        'top-end',
-        'bottom',
-        'bottom-start',
-        'bottom-end',
-        'end',
-        'end-top',
-        'end-bottom',
-        'start',
-        'start-top',
-        'start-bottom'
-      ]
-    }
+  isAnimated: {
+    control: 'boolean'
   },
   hasArrow: {
     control: 'boolean'
@@ -102,5 +159,11 @@ Default.argTypes = {
   },
   id: {
     control: 'text'
+  },
+  referenceElement: { control: { disable: true } },
+  popperModifiers: { control: { disable: true } },
+  backdropProps: { control: { disable: true } },
+  placement: {
+    control: { disable: true }
   }
 };
