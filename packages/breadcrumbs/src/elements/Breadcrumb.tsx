517b1e9c9eb8a53d28e196c5b8d59456d24a881e
KICKdesigns <jwang@KICKdesigns.com>
2020-11-05 08:56:34 -0800

    chore(breadcrumbs): add extends clause (#927)

--- packages/breadcrumbs/src/elements/Breadcrumb.tsx
+++ packages/breadcrumbs/src/elements/Breadcrumb.tsx
@@ -14,6 +14,9 @@ import {
   StyledChevronIcon
 } from '../styled';
 
+/**
+ * @extends HTMLAttributes<HTMLElement>
+ */
 export const Breadcrumb = React.forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>(
   (props, ref) => {
     const { getContainerProps, getCurrentPageProps } = useBreadcrumb();
