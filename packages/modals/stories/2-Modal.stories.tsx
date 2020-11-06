1202196c89f634d630567bdbeeb381ee94d1eef1
hzhu <hz@henryzhu.me>
2020-11-06 08:15:10 -0800

    Add long scrollable text

--- packages/modals/stories/2-Modal.stories.tsx
+++ packages/modals/stories/2-Modal.stories.tsx
@@ -8,6 +8,7 @@
 import React, { useState } from 'react';
 import { Story, Meta } from '@storybook/react';
 import { Button } from '@zendeskgarden/react-buttons';
+import { Paragraph } from '@zendeskgarden/react-typography';
 import { Modal, Header, Body, Footer, FooterItem, Close } from '@zendeskgarden/react-modals';
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
 
@@ -46,18 +47,120 @@ export const Default: Story = ({
             >
               <Header isDanger={isDanger}>Brussels Sprout</Header>
               <Body>
-                Soko radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root sea
-                lettuce brussels sprout cabbage.
-                {isLarge && (
+                {isLarge ? (
                   <>
-                    {' '}
-                    Catsear cauliflower garbanzo yarrow salsify chicory garlic bell pepper napa
-                    cabbage lettuce. Tomato kale arugula melon sierra leone bologi rutabaga
-                    tigernut. Sea lettuce gumbo grape kale kombu cauliflower salsify kohlrabi okra
-                    sea lettuce broccoli celery lotus root carrot winter purslane turnip greens
-                    garlic. Jícama garlic courgette coriander radicchio plantain scallion
-                    cauliflower fava bean desert raisin spring onion chicory bunya nuts. Sea lettuce
-                    water spinach gram fava bean leek dandelion silver beet eggplant bush.
+                    <Paragraph>
+                      Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi
+                      amaranth water spinach avocado daikon napa cabbage asparagus winter purslane
+                      kale. Celery potato scallion desert raisin horseradish spinach carrot soko.
+                      Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss
+                      chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander
+                      water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress.
+                      Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper
+                      artichoke. Nori grape silver beet broccoli kombu beet greens fava bean potato
+                      quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip
+                      greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane
+                      fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce
+                      parsley jícama. Celery quandong swiss chard chicory earthnut pea potato.
+                      Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens
+                      nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels
+                      sprout chard. Pea horseradish azuki bean lettuce avocado asparagus okra.
+                      Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green
+                      bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea. Soko
+                      radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root
+                      sea lettuce brussels sprout cabbage. Catsear cauliflower garbanzo yarrow
+                      salsify chicory garlic bell pepper napa cabbage lettuce tomato kale arugula
+                      melon sierra leone bologi rutabaga tigernut.
+                    </Paragraph>
+                    <Paragraph>
+                      Sea lettuce gumbo grape kale kombu cauliflower salsify kohlrabi okra sea
+                      lettuce broccoli celery lotus root carrot winter purslane turnip greens
+                      garlic. Jícama garlic courgette coriander radicchio plantain scallion
+                      cauliflower fava bean desert raisin spring onion chicory bunya nuts. Sea
+                      lettuce water spinach gram fava bean leek dandelion silver beet eggplant bush.
+                      Brussels sprout coriander water chestnut gourd swiss chard wakame kohlrabi
+                      beetroot carrot watercress. Corn amaranth salsify bunya nuts nori azuki bean
+                      chickweed potato bell pepper artichoke. Nori grape silver beet broccoli kombu
+                      beet greens fava bean potato quandong celery. Bunya nuts black-eyed pea
+                      prairie turnip leek lentil turnip greens parsnip. Sea lettuce lettuce water
+                      chestnut eggplant winter purslane fennel azuki bean earthnut pea sierra leone
+                      bologi leek soko chicory celtuce parsley jícama. Celery quandong swiss chard
+                      chicory earthnut pea potato. Salsify taro catsear garlic gram celery
+                      bitterleaf wattle seed collard greens nori. Grape wattle seed kombu beetroot
+                      horseradish carrot squash brussels sprout chard. Pea horseradish azuki bean
+                      lettuce avocado asparagus okra. Kohlrabi radish okra azuki bean corn fava bean
+                      mustard tigernut jícama green bean celtuce collard greens avocado quandong
+                      fennel gumbo black-eyed pea. Soko radicchio bunya nuts gram dulse silver beet
+                      parsnip napa cabbage lotus root sea lettuce brussels sprout cabbage. Catsear
+                      cauliflower garbanzo yarrow salsify chicory garlic bell pepper napa cabbage
+                      lettuce tomato kale arugula melon sierra leone bologi rutabaga tigernut. Sea
+                      lettuce gumbo grape kale kombu cauliflower salsify kohlrabi okra sea lettuce
+                      broccoli celery lotus root carrot winter purslane turnip greens garlic. Jícama
+                      garlic courgette coriander radicchio plantain scallion cauliflower fava bean
+                      desert raisin spring onion chicory bunya nuts. Sea lettuce water spinach gram
+                      fava bean leek dandelion silver beet eggplant bush.
+                    </Paragraph>
+                    <Paragraph>
+                      Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi
+                      amaranth water spinach avocado daikon napa cabbage asparagus winter purslane
+                      kale. Celery potato scallion desert raisin horseradish spinach carrot soko.
+                      Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss
+                      chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander
+                      water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress.
+                      Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper
+                      artichoke. Nori grape silver beet broccoli kombu beet greens fava bean potato
+                      quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip
+                      greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane
+                      fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce
+                      parsley jícama. Celery quandong swiss chard chicory earthnut pea potato.
+                      Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens
+                      nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels
+                      sprout chard. Pea horseradish azuki bean lettuce avocado asparagus okra.
+                      Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green
+                      bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea. Soko
+                      radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root
+                      sea lettuce brussels sprout cabbage. Catsear cauliflower garbanzo yarrow
+                      salsify chicory garlic bell pepper napa cabbage lettuce tomato kale arugula
+                      melon sierra leone bologi rutabaga tigernut. Sea lettuce gumbo grape kale
+                      kombu cauliflower salsify kohlrabi okra sea lettuce broccoli celery lotus root
+                      carrot winter purslane turnip greens garlic. Jícama garlic courgette coriander
+                      radicchio plantain scallion cauliflower fava bean desert raisin spring onion
+                      chicory bunya nuts. Sea lettuce water spinach gram fava bean leek dandelion
+                      silver beet eggplant bush.
+                    </Paragraph>
+                    <Paragraph>
+                      Turnip greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi
+                      amaranth water spinach avocado daikon napa cabbage asparagus winter purslane
+                      kale. Celery potato scallion desert raisin horseradish spinach carrot soko.
+                      Lotus root water spinach fennel kombu maize bamboo shoot green bean swiss
+                      chard seakale pumpkin onion chickpea gram corn pea. Brussels sprout coriander
+                      water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress.
+                      Corn amaranth salsify bunya nuts nori azuki bean chickweed potato bell pepper
+                      artichoke. Nori grape silver beet broccoli kombu beet greens fava bean potato
+                      quandong celery. Bunya nuts black-eyed pea prairie turnip leek lentil turnip
+                      greens parsnip. Sea lettuce lettuce water chestnut eggplant winter purslane
+                      fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce
+                      parsley jícama. Celery quandong swiss chard chicory earthnut pea potato.
+                      Salsify taro catsear garlic gram celery bitterleaf wattle seed collard greens
+                      nori. Grape wattle seed kombu beetroot horseradish carrot squash brussels
+                      sprout chard. Pea horseradish azuki bean lettuce avocado asparagus okra.
+                      Kohlrabi radish okra azuki bean corn fava bean mustard tigernut jícama green
+                      bean celtuce collard greens avocado quandong fennel gumbo black-eyed pea. Soko
+                      radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root
+                      sea lettuce brussels sprout cabbage. Catsear cauliflower garbanzo yarrow
+                      salsify chicory garlic bell pepper napa cabbage lettuce tomato kale arugula
+                      melon sierra leone bologi rutabaga tigernut. Sea lettuce gumbo grape kale
+                      kombu cauliflower salsify kohlrabi okra sea lettuce broccoli celery lotus root
+                      carrot winter purslane turnip greens garlic. Jícama garlic courgette coriander
+                      radicchio plantain scallion cauliflower fava bean desert raisin spring onion
+                      chicory bunya nuts. Sea lettuce water spinach gram fava bean leek dandelion
+                      silver beet eggplant bush.
+                    </Paragraph>
+                  </>
+                ) : (
+                  <>
+                    Soko radicchio bunya nuts gram dulse silver beet parsnip napa cabbage lotus root
+                    sea lettuce brussels sprout cabbage.
                   </>
                 )}
               </Body>
