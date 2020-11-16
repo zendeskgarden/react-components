08eb148eec47c58f2e1e46f67a775b1c8ba7bdf7
Jonathan Zempel <jzempel@gmail.com>
2020-11-16 11:20:01 -0800

    Add Blockquote component & example

---
+++ packages/typography/src/styled/StyledBlockquote.ts
@@ -0,0 +1,33 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import styled from 'styled-components';
+import { DEFAULT_THEME, getColor, retrieveComponentStyles } from '@zendeskgarden/react-theming';
+
+const COMPONENT_ID = 'typography.blockquote';
+
+export const StyledBlockquote = styled.blockquote.attrs({
+  'data-garden-id': COMPONENT_ID,
+  'data-garden-version': PACKAGE_VERSION
+})`
+  margin: 0;
+  /* stylelint-disable property-no-unknown */
+  border-${props => (props.theme.rtl ? 'right' : 'left')}: ${props =>
+  props.theme.shadowWidths.sm} solid;
+  border-color: ${props => getColor('neutralHue', 400, props.theme)};
+  padding: 0;
+  padding-${props => (props.theme.rtl ? 'right' : 'left')}: ${props =>
+  props.theme.space.base * 4}px;
+  /* stylelint-enable property-no-unknown */
+  direction: ${props => (props.theme.rtl ? 'rtl' : 'ltr')};
+
+  ${props => retrieveComponentStyles(COMPONENT_ID, props)};
+`;
+
+StyledBlockquote.defaultProps = {
+  theme: DEFAULT_THEME
+};
