e6df514bbe5da8a321f8844fba9a328811d6afcb
Henry Zhu <hz@henryzhu.me>
2020-11-17 13:28:22 -0800

    fix(loaders): fix Dot loader for IE11 (#936)

--- packages/loaders/src/elements/Dots.tsx
+++ packages/loaders/src/elements/Dots.tsx
@@ -5,9 +5,11 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 import PropTypes from 'prop-types';
+import { ThemeContext } from 'styled-components';
 import { useSchedule } from '@zendeskgarden/container-schedule';
+import { useDocument } from '@zendeskgarden/react-theming';
 import {
   StyledDotsCircleOne,
   StyledDotsCircleTwo,
@@ -15,7 +17,6 @@ import {
   StyledSVG,
   StyledLoadingPlaceholder
 } from '../styled';
-import { useCSSSVGAnimation } from '../utils/useCSSSVGAnimation';
 
 const COMPONENT_ID = 'loaders.dots';
 
@@ -39,14 +40,23 @@ export interface IDotsProps extends React.HTMLAttributes<SVGSVGElement> {
 }
 
 const Dots: React.FC<IDotsProps> = ({ size, color, duration, delayMS, ...other }) => {
-  const noAnimatedSVGSupport = useCSSSVGAnimation();
-  const { delayComplete } = useSchedule({ duration, delayMS, loop: noAnimatedSVGSupport });
+  const theme = useContext(ThemeContext);
+  const environment = useDocument(theme);
+  const canTransformSVG = useRef<boolean | null>(null);
+
+  if (environment && canTransformSVG.current === null) {
+    const svg = environment.createElementNS('http://www.w3.org/2000/svg', 'svg');
+
+    canTransformSVG.current = 'transform' in svg;
+  }
+
+  const { delayComplete } = useSchedule({ duration, delayMS, loop: true });
   const dotOne = useRef<SVGCircleElement>(null);
   const dotTwo = useRef<SVGCircleElement>(null);
   const dotThree = useRef<SVGCircleElement>(null);
 
   useEffect(() => {
-    if (noAnimatedSVGSupport && delayComplete) {
+    if (!canTransformSVG.current && delayComplete) {
       const transforms = [
         window.getComputedStyle(dotOne.current!).getPropertyValue('transform'),
         window.getComputedStyle(dotTwo.current!).getPropertyValue('transform'),
