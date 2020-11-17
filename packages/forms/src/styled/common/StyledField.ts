ef640f894626a03549dbe824946dcd2d1201e4f7
Jonathan Zempel <jzempel@gmail.com>
2020-11-17 15:12:44 -0800

    fix(forms): restore relative positioning context on Field container (#940)

--- packages/forms/src/styled/common/StyledField.ts
+++ packages/forms/src/styled/common/StyledField.ts
@@ -10,15 +10,19 @@ import { retrieveComponentStyles, DEFAULT_THEME } from '@zendeskgarden/react-the
 
 const COMPONENT_ID = 'forms.field';
 
+/**
+ * 1. Set positioning context for absolute contents (Checkbox, Radio, Toggle).
+ * 2. Resets for <fieldset>.
+ */
 export const StyledField = styled.div.attrs({
   'data-garden-id': COMPONENT_ID,
   'data-garden-version': PACKAGE_VERSION
 })`
+  position: relative; /* [1] */
   direction: ${props => props.theme.rtl && 'rtl'};
-  /* as <fieldset> resets */
-  margin: 0;
-  border: 0;
-  padding: 0;
+  margin: 0; /* [2] */
+  border: 0; /* [2] */
+  padding: 0; /* [2] */
 
   ${props => retrieveComponentStyles(COMPONENT_ID, props)};
 `;
