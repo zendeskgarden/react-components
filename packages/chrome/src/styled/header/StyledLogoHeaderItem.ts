5b813ae810c31543b8da89cafc53dc99d2cd08af
Jonathan Zempel <jzempel@gmail.com>
2020-12-17 12:15:58 -0800

    Add cross-reference link

--- packages/chrome/src/styled/header/StyledLogoHeaderItem.ts
+++ packages/chrome/src/styled/header/StyledLogoHeaderItem.ts
@@ -21,7 +21,11 @@ import { getNavWidth } from '../nav/StyledNav';
 const COMPONENT_ID = 'chrome.header_item';
 
 export interface IStyledLogoHeaderItemProps {
-  /** Applies a brand color to the product logo */
+  /**
+   * Applies a
+   * [brand color](https://garden.zendesk.com/design/color#brand-colors)
+   * to the product logo
+   */
   product?: PRODUCT;
 }
 
