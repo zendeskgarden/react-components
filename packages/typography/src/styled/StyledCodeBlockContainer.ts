c2870046a1743d93147b9715e2a216cf77097111
Jonathan Zempel <jzempel@gmail.com>
2021-02-04 17:06:44 -0800

    Wrap with an overflow container and auto-apply tab index

---
+++ packages/typography/src/styled/StyledCodeBlockContainer.ts
@@ -0,0 +1,24 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import styled from 'styled-components';
+import { DEFAULT_THEME, retrieveComponentStyles } from '@zendeskgarden/react-theming';
+
+const COMPONENT_ID = 'typography.codeblock_container';
+
+export const StyledCodeBlockContainer = styled.div.attrs({
+  'data-garden-id': COMPONENT_ID,
+  'data-garden-version': PACKAGE_VERSION
+})`
+  overflow: auto;
+
+  ${props => retrieveComponentStyles(COMPONENT_ID, props)};
+`;
+
+StyledCodeBlockContainer.defaultProps = {
+  theme: DEFAULT_THEME
+};
