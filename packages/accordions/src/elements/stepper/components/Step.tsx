93d4d8d41a1bb33529e139b58ba9204735422b31
MarkSFrancis <mrkfrancis8@gmail.com>
2021-02-08 12:18:10 +0000

    Removed unecessary update for setIndex in cleanup

--- packages/accordions/src/elements/stepper/components/Step.tsx
+++ packages/accordions/src/elements/stepper/components/Step.tsx
@@ -5,21 +5,13 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { forwardRef, LiHTMLAttributes, useEffect, useRef, useState } from 'react';
+import React, { forwardRef, LiHTMLAttributes, useEffect, useState } from 'react';
 import { StyledStep, StyledLine } from '../../../styled';
 import { StepContext, useStepperContext } from '../../../utils';
 
 const useStep = () => {
   const { currentIndexRef, ...context } = useStepperContext();
   const [index, setIndex] = useState(currentIndexRef.current);
-  const isMounted = useRef(true);
-
-  useEffect(
-    () => () => {
-      isMounted.current = false;
-    },
-    []
-  );
 
   useEffect(() => {
     setIndex(currentIndexRef.current);
@@ -28,9 +20,6 @@ const useStep = () => {
 
     return () => {
       currentIndex.current--;
-      if (isMounted.current) {
-        setIndex(currentIndex.current);
-      }
     };
   }, [currentIndexRef]);
 
