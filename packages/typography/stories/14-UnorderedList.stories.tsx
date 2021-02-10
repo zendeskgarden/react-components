82d5edf7b8ff1b180a3c984015c9b9f9a19f3d36
Jonathan Zempel <jzempel@gmail.com>
2021-02-09 17:01:39 -0800

    Update list storybook examples

--- packages/typography/stories/14-UnorderedList.stories.tsx
+++ packages/typography/stories/14-UnorderedList.stories.tsx
@@ -15,28 +15,74 @@ export default {
   component: UnorderedList
 } as Meta;
 
-export const Default: Story = ({ size, type }) => (
+const text = [
+  `garden es bonus vobis proinde vos postulo essum magis kohlrabi welsh onion daikon amaranth
+  tatsoi tomatillo melon azuki bean garlic beet greens corn soko endive gumbo gourd shallot
+  courgette tatsoi pea sprouts fava bean collard greens dandelion okra wakame tomato cucumber
+  earthnut pea peanut soko zucchini.`.split(' '),
+  `greens yarrow ricebean rutabaga endive cauliflower sea lettuce kohlrabi amaranth water spinach
+  avocado daikon napa cabbage asparagus winter purslane kale celery potato scallion desert raisin
+  horseradish spinach carrot soko lotus root water spinach fennel kombu maize bamboo shoot green
+  bean swiss chard seakale pumpkin sprout coriander.`.split(' '),
+  `water chestnut gourd swiss chard wakame kohlrabi beetroot carrot watercress corn amaranth
+  salsify bunya nuts nori azuki bean chickweed potato bell pepper artichoke chestnut eggplant
+  winter purslane fennel azuki bean earthnut pea sierra leone bologi leek soko chicory celtuce
+  parsley jícama salsify celery quandong swiss chard.`.split(' '),
+  `rock melon radish asparagus spinach beetroot water spinach okra water chestnut ricebean pea
+  catsear courgette summer purslane water spinach arugula pea tatsoi aubergine spring onion bush
+  tomato kale radicchio turnip chicory salsify pea sprouts fava bean dandelion zucchini burdock
+  yarrow chickpea dandelion sorrel courgette turnip greens.`.split(' ')
+];
+
+type TYPE = 'disc' | 'circle' | 'square';
+
+const getType = (level: number): TYPE => {
+  const types: TYPE[] = ['disc', 'circle', 'square'];
+  const index = level % types.length;
+
+  return types[index];
+};
+
+const NestedList = ({ length = 1, levels = 1, level = 0, ...props }) => {
+  const content = text.map(string => string.slice(0, length).join(' '));
+
+  if (level < levels) {
+    return (
+      <UnorderedList {...props} type={level === 0 ? props.type : getType(level)}>
+        <UnorderedList.Item>{content[0]}</UnorderedList.Item>
+        <UnorderedList.Item>
+          {content[1]}
+          <NestedList length={length} levels={levels} level={level + 1} {...props} />
+        </UnorderedList.Item>
+        {level === 0 && (
+          <>
+            <UnorderedList.Item>{content[2]}</UnorderedList.Item>
+            <UnorderedList.Item>{content[3]}</UnorderedList.Item>
+          </>
+        )}
+      </UnorderedList>
+    );
+  }
+
+  return null;
+};
+
+export const Default: Story = props => (
   <Grid>
-    <Row justifyContent="center">
-      <Col size={2}>
-        <UnorderedList size={size} type={type}>
-          <UnorderedList.Item>Plant</UnorderedList.Item>
-          <UnorderedList.Item>Water</UnorderedList.Item>
-          <UnorderedList.Item>Harvest</UnorderedList.Item>
-        </UnorderedList>
+    <Row>
+      <Col>
+        <NestedList {...props} />
       </Col>
     </Row>
   </Grid>
 );
 
+Default.args = {
+  length: 1,
+  levels: 1
+};
+
 Default.argTypes = {
-  size: {
-    control: { type: 'select', options: ['small', 'medium', 'large'] }
-  },
-  type: {
-    control: {
-      type: 'select',
-      options: ['disc', 'circle', 'square']
-    }
-  }
+  length: { control: { type: 'range', min: 1, max: text[0].length } },
+  levels: { control: { type: 'range', min: 1, max: 9 } }
 };
