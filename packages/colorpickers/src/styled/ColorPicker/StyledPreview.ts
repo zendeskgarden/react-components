ed281dfb4c53f20b09064ab20184091162dd8630
hzhu <hz@henryzhu.me>
2021-02-18 13:56:23 -0800

    Refactor StyledPreview to use theme.borderWidths

--- packages/colorpickers/src/styled/ColorPicker/StyledPreview.ts
+++ packages/colorpickers/src/styled/ColorPicker/StyledPreview.ts
@@ -24,7 +24,7 @@ export const StyledPreview = styled.div.attrs<IRGBColor>(props => ({
   flex-shrink: 0;
   border-radius: ${props => props.theme.borderRadii.md};
   /* stylelint-disable-next-line color-function-notation */
-  box-shadow: inset 0 0 0 1px
+  box-shadow: inset 0 0 0 ${props => props.theme.borderWidths.sm}
     ${props => getColor(props.theme.palette.black, 600, props.theme, 0.19)};
   width: ${props => props.theme.space.base * 8}px;
   height: ${props => props.theme.space.base * 8}px;
