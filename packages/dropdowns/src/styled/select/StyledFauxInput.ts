b9ffa85c22a6a38979769e7e167c24253b6040e2
Jonathan Zempel <jzempel@gmail.com>
2021-03-25 10:55:27 -0700

    Rollback StyledFauxInput test

--- packages/dropdowns/src/styled/select/StyledFauxInput.ts
+++ packages/dropdowns/src/styled/select/StyledFauxInput.ts
@@ -11,12 +11,11 @@ import { FauxInput } from '@zendeskgarden/react-forms';
 
 const COMPONENT_ID = 'dropdowns.faux_input';
 
-export const StyledFauxInput = styled(FauxInput).attrs(props => ({
-  ...props,
+export const StyledFauxInput = styled(FauxInput).attrs({
   'data-garden-id': COMPONENT_ID,
   'data-garden-version': PACKAGE_VERSION,
   mediaLayout: true
-}))`
+})`
   cursor: ${props => !props.disabled && 'pointer'};
   min-width: ${props => props.theme.space.base * (props.isCompact ? 25 : 36)}px;
 
