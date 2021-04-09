163785c60aa861b58e85238ed2c52536fbbb9e5d
hzhu <hz@henryzhu.me>
2021-04-09 12:44:31 -0700

    reverse the font-weight and use regular weight by default

--- packages/forms/src/styled/radio/StyledRadioLabel.ts
+++ packages/forms/src/styled/radio/StyledRadioLabel.ts
@@ -40,6 +40,8 @@ export const StyledRadioLabel = styled(StyledLabel).attrs({
   position: relative;
   cursor: pointer;
   user-select: none;
+  font-weight: ${props =>
+    props.isRegular === false ? props.theme.fontWeights.semibold : props.theme.fontWeights.regular};
 
   ${props => sizeStyles(props)};
 
