d9a31f179b6142dd895a2ddcc3859f7094247e57
Jonathan Zempel <jzempel@gmail.com>
2021-09-01 10:43:39 -0400

    Add new RTL test

---
+++ packages/colorpickers/src/styled/Colorpicker/StyledRGBAField.spec.tsx
@@ -0,0 +1,26 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { render, renderRtl } from 'garden-test-utils';
+import { StyledRGBAField } from './StyledRGBAField';
+
+describe('StyledRGBAField', () => {
+  it('renders default style', () => {
+    const { container } = render(<StyledRGBAField />);
+
+    expect(container.firstChild).toHaveStyleRule('margin-left', '6px');
+    expect(container.firstChild).not.toHaveStyleRule('margin-right');
+  });
+
+  it('renders RTL correctly', () => {
+    const { container } = renderRtl(<StyledRGBAField />);
+
+    expect(container.firstChild).toHaveStyleRule('margin-right', '6px');
+    expect(container.firstChild).not.toHaveStyleRule('margin-left');
+  });
+});
