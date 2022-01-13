201c72bdcd087a10ea67c5916423d25ca0701d66
Jonathan Zempel <jzempel@gmail.com>
2022-01-13 09:40:47 -0500

    Update notification element component + subcomponent type conventions

---
+++ packages/notifications/src/elements/toaster/ToastContext.ts
@@ -0,0 +1,16 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import { createContext, Dispatch } from 'react';
+import { IToasterState, ToasterReducerAction } from './reducer';
+
+interface IToastContext {
+  state: IToasterState;
+  dispatch: Dispatch<ToasterReducerAction>;
+}
+
+export const ToastContext = createContext<IToastContext | undefined>(undefined);
