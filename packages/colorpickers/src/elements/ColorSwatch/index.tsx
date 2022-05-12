b23772e7d49aab69ccc20b668d281e214fdc7632
Jonathan Zempel <jzempel@gmail.com>
2022-05-12 17:31:28 -0400

    Update with latest useGrid hook

--- packages/colorpickers/src/elements/ColorSwatch/index.tsx
+++ packages/colorpickers/src/elements/ColorSwatch/index.tsx
@@ -5,59 +5,121 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { forwardRef, useContext } from 'react';
+import React, { FocusEventHandler, forwardRef, useContext, useRef, useState } from 'react';
 import PropTypes from 'prop-types';
+import mergeRefs from 'react-merge-refs';
 import { ThemeContext } from 'styled-components';
 import { Tooltip } from '@zendeskgarden/react-tooltips';
 import { useGrid } from '@zendeskgarden/container-grid';
 import { useId } from '@zendeskgarden/container-utilities';
-import CheckIcon from '@zendeskgarden/svg-icons/src/12/check-sm-fill.svg';
-import { StyledSwatchButton, StyledIcon, StyledCell, StyledColorSwatch } from '../../styled';
+import { StyledCell, StyledColorSwatch } from '../../styled';
 import { IColorSwatchProps, ILabeledColor } from '../../types';
 
 /**
  * @extends HTMLAttributes<HTMLTableElement>
  */
 export const ColorSwatch = forwardRef<HTMLTableElement, IColorSwatchProps>(
-  ({ colors, ...props }, ref) => {
+  (
+    {
+      colors,
+      defaultSelectedColIndex,
+      defaultSelectedRowIndex,
+      selectedColIndex,
+      selectedRowIndex,
+      onSelect,
+      ...props
+    },
+    ref
+  ) => {
     const { rtl } = useContext(ThemeContext);
+    const gridRef = useRef<HTMLTableElement>();
+    const [rowIndex, setRowIndex] = useState(selectedRowIndex);
+    const [colIndex, setColIndex] = useState(selectedColIndex);
+    const isControlled = selectedColIndex !== undefined && selectedRowIndex !== undefined;
     const { getGridCellProps } = useGrid({
       rtl,
       matrix: colors,
-      selection: true,
       wrap: true,
       idPrefix: useId(),
-      ...props
+      defaultRowIndex: defaultSelectedRowIndex,
+      defaultColIndex: defaultSelectedColIndex,
+      rowIndex,
+      colIndex,
+      onChange: (row, col) => {
+        setRowIndex(row);
+        setColIndex(col);
+      }
     });
 
     return (
-      <StyledColorSwatch ref={ref}>
+      <StyledColorSwatch role="grid" ref={mergeRefs([gridRef, ref])} {...props}>
         <tbody>
           {colors.map((row: ILabeledColor[], rowIdx: number) => (
             <tr key={row[0].value}>
               {row.map((color: ILabeledColor, colIdx: number) => {
                 const { label, value } = color;
-                const { 'aria-selected': ariaSelected, ...other } = getGridCellProps({
+                const { role, ...gridCellProps } = getGridCellProps({
                   colIdx,
-                  rowIdx,
-                  type: 'button',
-                  role: undefined
+                  rowIdx
                 });
+                let checked;
+                let handleChange;
+                let defaultChecked;
+
+                if (isControlled) {
+                  checked = selectedRowIndex === rowIdx && selectedColIndex === colIdx;
+                  handleChange = () => onSelect && onSelect(rowIdx, colIdx);
+                } else {
+                  defaultChecked =
+                    defaultSelectedRowIndex === rowIdx && defaultSelectedColIndex === colIdx;
+                }
+
+                /**
+                 * When the ColorSwatch loses focus, reset the roving tab
+                 * index to the selected color. Otherwise, keyboard access to
+                 * the native radio group is lost.
+                 */
+                const handleBlur: FocusEventHandler<HTMLInputElement> = event => {
+                  if (!gridRef.current?.contains(event.relatedTarget)) {
+                    const selectedInput = document.querySelector(
+                      `input[name='${event.target.name}']:checked`
+                    );
+
+                    if (selectedInput !== null) {
+                      const inputs = document.getElementsByName(
+                        event.target.name
+                      ) as NodeListOf<HTMLInputElement>;
+
+                      inputs.forEach(input =>
+                        input.setAttribute('tabIndex', input.checked ? '0' : '-1')
+                      );
+                    }
+                  }
+                };
 
                 return (
-                  <StyledCell key={value} aria-selected={ariaSelected}>
-                    <Tooltip content={label}>
-                      <StyledSwatchButton
-                        backgroundColor={value}
-                        aria-pressed={ariaSelected}
-                        aria-label={label}
-                        {...other}
-                      >
-                        <StyledIcon color={value} selected={ariaSelected}>
+                  <StyledCell key={value} role={role}>
+                    <label>
+                      <Tooltip content={label}>
+                        <input
+                          name="name"
+                          type="radio"
+                          value={value}
+                          defaultChecked={defaultChecked}
+                          checked={checked}
+                          onBlur={handleBlur}
+                          onChange={handleChange}
+                          {...gridCellProps}
+                        />
+                      </Tooltip>
+                    </label>
+                    {/* <Tooltip content={label}>
+                      <StyledSwatchButton backgroundColor={value} aria-label={label} {...other}>
+                        <StyledIcon color={value} selected={checked || defaultChecked}>
                           <CheckIcon />
                         </StyledIcon>
                       </StyledSwatchButton>
-                    </Tooltip>
+                    </Tooltip> */}
                   </StyledCell>
                 );
               })}
@@ -73,14 +135,9 @@ ColorSwatch.displayName = 'ColorSwatch';
 
 ColorSwatch.propTypes = {
   colors: PropTypes.arrayOf(PropTypes.any).isRequired,
-  rowIndex: PropTypes.number,
-  colIndex: PropTypes.number,
   selectedRowIndex: PropTypes.number,
   selectedColIndex: PropTypes.number,
-  defaultRowIndex: PropTypes.number,
-  defaultColIndex: PropTypes.number,
   defaultSelectedRowIndex: PropTypes.number,
   defaultSelectedColIndex: PropTypes.number,
-  onChange: PropTypes.func,
   onSelect: PropTypes.func
 };
