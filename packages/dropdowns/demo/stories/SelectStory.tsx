906d2e2e8edf0cea89d5398a626cb3d17973d2bd
Mike Tobia <mike.tobia@zendesk.com>
2022-07-14 15:43:09 -0400

    chore(dropdowns): update storybook

--- packages/dropdowns/demo/stories/SelectStory.tsx
+++ packages/dropdowns/demo/stories/SelectStory.tsx
@@ -10,20 +10,13 @@ import { Story } from '@storybook/react';
 import Icon from '@zendeskgarden/svg-icons/src/16/leaf-stroke.svg';
 import { IDropdownProps, IMenuProps, ISelectProps, Select } from '@zendeskgarden/react-dropdowns';
 import { DropdownFieldStory } from './DropdownFieldStory';
-import { IMenuItem } from './types';
+import { ICommonArgs, IMenuItem } from './types';
 
-interface IArgs extends ISelectProps {
+interface IArgs extends ISelectProps, ICommonArgs {
   downshiftProps?: IDropdownProps['downshiftProps'];
   selectedItem: IDropdownProps['selectedItem'];
   onStateChange: IDropdownProps['onStateChange'];
   isOpen?: IDropdownProps['isOpen'];
-  label?: string;
-  isLabelRegular?: boolean;
-  isLabelHidden?: boolean;
-  hasHint?: boolean;
-  hint?: string;
-  hasMessage?: boolean;
-  message?: string;
   hasIcon: boolean;
   items: IMenuItem[];
   placement: IMenuProps['placement'];
@@ -63,6 +56,7 @@ export const SelectStory: Story<IArgs> = ({
     hasMessage={hasMessage}
     message={message}
     validation={args.validation}
+    validationLabel={args.validationLabel}
     menuProps={{ isCompact: args.isCompact, placement }}
     items={items}
     itemProps={{ hasIcon, disabled: args.disabled }}
