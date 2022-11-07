90aa8cc75e4bae614f565c533a778a074579c08b
Jen Gorfine (she/her) <93289772+jgorfine-zendesk@users.noreply.github.com>
2022-11-07 13:45:42 -0500

    feat(anchor): add default alternative text to external anchor icon (#1448)

--- packages/buttons/src/elements/Anchor.tsx
+++ packages/buttons/src/elements/Anchor.tsx
@@ -9,12 +9,13 @@ import React, { AnchorHTMLAttributes, forwardRef } from 'react';
 import PropTypes from 'prop-types';
 import { IAnchorProps } from '../types';
 import { StyledAnchor, StyledExternalIcon } from '../styled';
+import { useText } from '@zendeskgarden/react-theming';
 
 /**
  * @extends AnchorHTMLAttributes<HTMLAnchorElement>
  */
 export const Anchor = forwardRef<HTMLAnchorElement, IAnchorProps>(
-  ({ children, isExternal, ...otherProps }, ref) => {
+  ({ children, isExternal, externalIconLabel, ...otherProps }, ref) => {
     let anchorProps: AnchorHTMLAttributes<HTMLAnchorElement> = otherProps;
 
     if (isExternal) {
@@ -25,10 +26,21 @@ export const Anchor = forwardRef<HTMLAnchorElement, IAnchorProps>(
       };
     }
 
+    // Only show label warnings for the external icon when the Anchor is external
+    const checkProps = isExternal ? { externalIconLabel } : { noIconLabel: 'true' };
+    const iconAriaLabel = useText(
+      Anchor,
+      checkProps,
+      isExternal ? 'externalIconLabel' : 'noIconLabel',
+      '(opens in a new tab)'
+    );
+
     return (
       <StyledAnchor ref={ref} {...(anchorProps as any)}>
         {children}
-        {isExternal && <StyledExternalIcon />}
+        {isExternal && (
+          <StyledExternalIcon role="img" aria-label={iconAriaLabel} aria-hidden={undefined} />
+        )}
       </StyledAnchor>
     );
   }
@@ -38,5 +50,6 @@ Anchor.displayName = 'Anchor';
 
 Anchor.propTypes = {
   isExternal: PropTypes.bool,
-  isDanger: PropTypes.bool
+  isDanger: PropTypes.bool,
+  externalIconLabel: PropTypes.string
 };
