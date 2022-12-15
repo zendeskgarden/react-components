1945c2b70c19070f7052bdfc6591f357cb738dbb
Mike Tobia <mike.tobia@zendesk.com>
2022-12-15 10:37:27 -0500

    test(table): add styled head spec

---
+++ packages/tables/src/styled/StyledHead.spec.tsx
@@ -0,0 +1,39 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { render } from 'garden-test-utils';
+
+import { StyledHead } from './StyledHead';
+import { StyledHeaderRow } from './StyledHeaderRow';
+
+describe('StyledHead', () => {
+  it('renders the expected element', () => {
+    const { container } = render(
+      <table>
+        <StyledHead />
+      </table>
+    );
+
+    expect(container.firstChild!.childNodes[0].nodeName).toBe('THEAD');
+  });
+
+  it('renders sticky head styles', () => {
+    const { getByTestId } = render(
+      <table>
+        <StyledHead isSticky data-test-id="head">
+          <StyledHeaderRow data-test-id="header" />
+        </StyledHead>
+      </table>
+    );
+
+    expect(getByTestId('head')).toHaveStyleRule('position', 'sticky');
+    expect(getByTestId('head')).toHaveStyleRule('border-bottom-color', 'transparent', {
+      modifier: `& > ${StyledHeaderRow}:last-child`
+    });
+  });
+});
