33efd5e18dd8a390466601fd2423f0c705f7f7e9
George Treviranus <3946669+geotrev@users.noreply.github.com>
2023-02-08 10:18:51 -0600

    fix: improves accessibility of Tag close button (#1511)

--- packages/tags/src/elements/Close.spec.tsx
+++ packages/tags/src/elements/Close.spec.tsx
@@ -16,8 +16,20 @@ describe('Close', () => {
     expect(container.querySelector('svg')).not.toBeNull();
   });
 
+  it('renders with default aria-label', () => {
+    const { container } = render(<Close />);
+
+    expect(container.firstChild).toHaveAttribute('aria-label', 'Remove');
+  });
+
+  it('renders with custom aria-label', () => {
+    const { container } = render(<Close aria-label="Delete" />);
+
+    expect(container.firstChild).toHaveAttribute('aria-label', 'Delete');
+  });
+
   it('passes ref to underlying DOM element', () => {
-    const ref = React.createRef<HTMLDivElement>();
+    const ref = React.createRef<HTMLButtonElement>();
     const { container } = render(<Close ref={ref} />);
 
     expect(container.firstChild).toBe(ref.current);
