b68095bfd16c107d93be0eb7e8d20244c6d449de
Mike Tobia <mike.tobia@zendesk.com>
2023-07-19 12:01:40 -0400

    fix: merge button forwarded ref with selection ref

--- packages/buttons/src/elements/Button.tsx
+++ packages/buttons/src/elements/Button.tsx
@@ -5,8 +5,9 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { forwardRef, SVGAttributes } from 'react';
+import React, { forwardRef, MutableRefObject, SVGAttributes } from 'react';
 import PropTypes from 'prop-types';
+import mergeRefs from 'react-merge-refs';
 import { IButtonProps, SIZE } from '../types';
 import { StyledButton } from '../styled';
 import { useButtonGroupContext } from '../utils/useButtonGroupContext';
@@ -25,6 +26,7 @@ const ButtonComponent = forwardRef<HTMLButtonElement, IButtonProps>((props, ref)
   const buttonGroupContext = useButtonGroupContext();
   const splitButtonContext = useSplitButtonContext();
 
+  let computedRef = ref;
   let computedProps = {
     ...props,
     focusInset: props.focusInset || buttonGroupContext !== undefined || splitButtonContext
@@ -39,9 +41,14 @@ const ButtonComponent = forwardRef<HTMLButtonElement, IButtonProps>((props, ref)
       isSelected: props.value === buttonGroupContext.selectedItem,
       ...computedProps
     });
+    computedRef = mergeRefs([
+      // a `ref` is added by the `useSelection` container which we need to merge with the forwarded `ref`
+      (computedProps as IButtonProps & { ref: MutableRefObject<HTMLButtonElement> }).ref,
+      ref
+    ]);
   }
 
-  return <StyledButton ref={ref} {...computedProps} />;
+  return <StyledButton ref={computedRef} {...computedProps} />;
 });
 
 ButtonComponent.displayName = 'Button';
