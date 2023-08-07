93306f81d4a051972ce8dd38cb62b07387b3ee45
Mike Tobia <mike.tobia@zendesk.com>
2023-08-07 10:52:41 -0400

    fix(accordions): enable dynamic height for panel

--- packages/accordions/src/elements/accordion/components/Panel.tsx
+++ packages/accordions/src/elements/accordion/components/Panel.tsx
@@ -5,11 +5,7 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { useEffect, useRef, useContext, forwardRef, HTMLAttributes } from 'react';
-import debounce from 'lodash.debounce';
-import mergeRefs from 'react-merge-refs';
-import { ThemeContext } from 'styled-components';
-import { useDocument } from '@zendeskgarden/react-theming';
+import React, { forwardRef, HTMLAttributes } from 'react';
 
 import { useAccordionContext, useSectionContext, IAccordionContext } from '../../../utils';
 import { StyledPanel, StyledInnerPanel } from '../../../styled';
@@ -17,47 +13,15 @@ import { StyledPanel, StyledInnerPanel } from '../../../styled';
 type PanelProps = IAccordionContext | { isExpanded?: boolean };
 
 const PanelComponent = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>((props, ref) => {
-  const { isCompact, isBare, isAnimated, getPanelProps, expandedSections } = useAccordionContext();
-  const panelRef = useRef<HTMLElement>();
+  const { isCompact, isBare, isAnimated, expandedSections, getPanelProps } = useAccordionContext();
   const index = useSectionContext();
   const isExpanded = expandedSections.includes(index);
-
-  const theme = useContext(ThemeContext);
-  const environment = useDocument(theme);
-
-  useEffect(() => {
-    if (!isAnimated) {
-      return undefined;
-    }
-
-    if (environment) {
-      const updateMaxHeight = debounce(() => {
-        if (panelRef.current) {
-          const child = panelRef.current.children[0] as any;
-
-          child.style.maxHeight = `${child.scrollHeight}px`;
-        }
-      }, 100);
-
-      const win = environment.defaultView! || window;
-
-      win.addEventListener('resize', updateMaxHeight);
-      updateMaxHeight();
-
-      return () => {
-        updateMaxHeight.cancel();
-        win.removeEventListener('resize', updateMaxHeight);
-      };
-    }
-
-    return undefined;
-  }, [isAnimated, panelRef, environment, props.children]);
 
   return (
     <StyledPanel
       {...getPanelProps<PanelProps>({
         role: null,
-        ref: mergeRefs([panelRef, ref]),
+        ref,
         index,
         isBare,
         isCompact,
@@ -66,9 +30,7 @@ const PanelComponent = forwardRef<HTMLElement, HTMLAttributes<HTMLElement>>((pro
         ...props
       })}
     >
-      <StyledInnerPanel isExpanded={isExpanded} isAnimated={isAnimated}>
-        {props.children}
-      </StyledInnerPanel>
+      <StyledInnerPanel>{props.children}</StyledInnerPanel>
     </StyledPanel>
   );
 });
