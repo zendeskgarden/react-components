e1a302eed5780193644481c13004a6ae19270d28
Jen Gorfine <jen.gorfine@zendesk.com>
2023-08-24 09:20:59 -0400

    feat: created StyledHiddenText component and spec

---
+++ packages/typography/src/styled/StyledHiddenText.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import styled from 'styled-components';
+import { hideVisually } from 'polished';
+import { retrieveComponentStyles, DEFAULT_THEME } from '@zendeskgarden/react-theming';
+
+const COMPONENT_ID = 'typography.hidden_text';
+
+export const StyledHiddenText = styled.span.attrs({
+  'data-garden-id': COMPONENT_ID,
+  'data-garden-version': PACKAGE_VERSION
+})`
+  ${hideVisually()}
+
+  ${props => retrieveComponentStyles(COMPONENT_ID, props)};
+`;
+
+StyledHiddenText.defaultProps = {
+  theme: DEFAULT_THEME
+};
