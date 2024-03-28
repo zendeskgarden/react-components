142778f45aa7f9d4925a1e76b9d622a0140c69b0
Jonathan Zempel <jzempel@gmail.com>
2024-03-28 11:03:58 -0400

    Add jsdoc

--- packages/theming/src/utils/getColor.ts
+++ packages/theming/src/utils/getColor.ts
@@ -149,6 +149,27 @@ type ColorParameters = {
   variable?: string;
 };
 
+/**
+ * Get a color value from the theme. Variable lookup takes precedence, followed
+ * by `dark` and `light` object values. If none of these are provided, `hue`,
+ * `shade`, `offset`, and `transparency` are used as fallbacks to determine the
+ * color.
+ *
+ * @param {Object} [options.theme] Provides values used to resolve the desired color
+ * @param {string} [options.variable] A variable key (i.e. `'background.default'`) used to resolve a color value for the theme color base
+ * @param {Object} [options.dark] An object with `hue`, `shade`, `offset`, and `transparency` values to be used in dark mode
+ * @param {Object} [options.light] An object with `hue`, `shade`, `offset`, and `transparency` values to be used in light mode
+ * @param {string} [options.hue] A `theme.palette` hue or one of the following `theme.colors` keys:
+ *  - `'primaryHue'` = `theme.colors.primaryHue`
+ *  - `'dangerHue'` = `theme.colors.dangerHue`
+ *  - `'warningHue'` = `theme.colors.warningHue`
+ *  - `'successHue'` = `theme.colors.successHue`
+ *  - `'neutralHue'` = `theme.colors.neutralHue`
+ *  - `'chromeHue'` = `theme.colors.chromeHue`
+ * @param {number} [options.shade] A hue shade
+ * @param {number} [options.offset] A positive or negative value to adjust the shade
+ * @param {number} [options.transparency] An alpha-channel value between 0 and 1
+ */
 export const getColor = memoize(
   ({ dark, hue, light, offset, shade, theme, transparency, variable }: ColorParameters) => {
     let retVal;
