f4e0e2f6389bcda8460c5ae1250d98f7c232380b
Florent <ze-flo@users.noreply.github.com>
2024-04-03 14:30:31 -0700

    build(browserslist): expand modern browser support, drop rare mobile browsers

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,8 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        targets: '> 0.5%, last 2 versions, Firefox ESR, not dead'
+        // https://browsersl.ist/#q=%3E+0.3%25%2C+last+2+versions%2C+Firefox+ESR%2C+not+dead%2C+not+and_qq+13-14%2C+not+kaios+2-5
+        targets: '> 0.3%, last 2 versions, Firefox ESR, not dead, not and_qq 13-14, not kaios 2-5'
       }
     ],
     [
@@ -18,7 +19,7 @@ module.exports = {
       {
         useBuiltIns: true // https://babeljs.io/docs/babel-preset-react#usebuiltins
       }
-    ], // https://babeljs.io/docs/babel-preset-react#usebuiltins
+    ],
     ['@babel/preset-typescript', { onlyRemoveTypeImports: true }]
   ],
   plugins: ['babel-plugin-styled-components'],
