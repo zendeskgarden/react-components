e3c3a6e46d0bda7b6fa5008cd0710433b5cec631
Florent <ze-flo@users.noreply.github.com>
2024-04-03 15:54:29 -0700

    Revert "build: create separate chunks for all modules to improve tree-shaking"
    This reverts commit 0bce1324794b60e9a90658ee5ef973ff7221b662.
    
    # Conflicts:
    #	utils/build/rollup.config.js

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -15,6 +15,7 @@ import typescript from 'rollup-plugin-typescript2';
 import { babel } from '@rollup/plugin-babel';
 // import { sizeSnapshot } from '@brodybits/rollup-plugin-size-snapshot';
 import analyze from 'rollup-plugin-analyzer';
+import license from 'rollup-plugin-license';
 import cleanup from 'rollup-plugin-cleanup';
 import del from 'rollup-plugin-delete';
 import svgr from '@svgr/rollup';
@@ -34,13 +35,6 @@ const externalPackages = [
   '@zendeskgarden/react-theming',
   ...Object.keys(pkg.dependencies || {})
 ].map(external => new RegExp(`^${external}/?.*`, 'u'));
-
-const BANNER = `/**
-* Copyright Zendesk, Inc.
-*
-* Use of this source code is governed under the Apache License, Version 2.0
-* found at http://www.apache.org/licenses/LICENSE-2.0.
-*/`;
 
 export default [
   {
@@ -74,7 +68,7 @@ export default [
         babelrc: false,
         exclude: 'node_modules/**', // only transpile our source code
         ...babelOptions,
-        extensions: ['.tsx', '.ts', ...DEFAULT_EXTENSIONS]
+        extensions: [...DEFAULT_EXTENSIONS, '.ts', '.tsx']
       }),
       /**
        * Replace PACKAGE_VERSION constant with the current package version
@@ -85,6 +79,17 @@ export default [
        */
       cleanup({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
       /**
+       * Apply global Zendesk license
+       */
+      license({
+        banner: `
+      Copyright Zendesk, Inc.
+
+      Use of this source code is governed under the Apache License, Version 2.0
+      found at http://www.apache.org/licenses/LICENSE-2.0.
+          `.trim()
+      }),
+      /**
        * Only enforce matching size snapshot files in CI environments
        */
       // sizeSnapshot({
@@ -94,20 +99,8 @@ export default [
       !!process.env.ANALYZE_BUNDLE && analyze({ summaryOnly: true })
     ],
     output: [
-      {
-        file: pkg.main,
-        format: 'cjs',
-        interop: 'auto',
-        banner: BANNER // Apply global Zendesk license
-      },
-      {
-        dir: path.dirname(pkg.module),
-        format: 'es',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
-        entryFileNames: '[name].js',
-        banner: BANNER // Apply global Zendesk license
-      }
+      { file: pkg.main, format: 'cjs', interop: 'auto' },
+      { file: pkg.module, format: 'es' }
     ]
   }
 ];
