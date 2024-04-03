783ef1328eceea7c4adee1c5b0a2e2bdea0b758b
Florent <ze-flo@users.noreply.github.com>
2024-04-03 14:22:57 -0700

    chore: use Rollup built-in option.banner

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -15,7 +15,6 @@ import typescript from 'rollup-plugin-typescript2';
 import { babel } from '@rollup/plugin-babel';
 // import { sizeSnapshot } from '@brodybits/rollup-plugin-size-snapshot';
 import analyze from 'rollup-plugin-analyzer';
-import license from 'rollup-plugin-license';
 import cleanup from 'rollup-plugin-cleanup';
 import del from 'rollup-plugin-delete';
 import svgr from '@svgr/rollup';
@@ -35,6 +34,13 @@ const externalPackages = [
   '@zendeskgarden/react-theming',
   ...Object.keys(pkg.dependencies || {})
 ].map(external => new RegExp(`^${external}/?.*`, 'u'));
+
+const BANNER = `/**
+* Copyright Zendesk, Inc.
+*
+* Use of this source code is governed under the Apache License, Version 2.0
+* found at http://www.apache.org/licenses/LICENSE-2.0.
+*/`;
 
 export default [
   {
@@ -79,17 +85,6 @@ export default [
        */
       cleanup({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
       /**
-       * Apply global Zendesk license
-       */
-      license({
-        banner: `
-      Copyright Zendesk, Inc.
-
-      Use of this source code is governed under the Apache License, Version 2.0
-      found at http://www.apache.org/licenses/LICENSE-2.0.
-          `.trim()
-      }),
-      /**
        * Only enforce matching size snapshot files in CI environments
        */
       // sizeSnapshot({
@@ -102,14 +97,16 @@ export default [
       {
         file: pkg.main,
         format: 'cjs',
-        interop: 'auto'
+        interop: 'auto',
+        banner: BANNER // Apply global Zendesk license
       },
       {
         dir: path.dirname(pkg.module),
         format: 'es',
         preserveModules: true,
         preserveModulesRoot: 'src',
-        entryFileNames: '[name].js'
+        entryFileNames: '[name].js',
+        banner: BANNER // Apply global Zendesk license
       }
     ]
   }
