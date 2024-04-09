9f3aa37d285099fb23bc2f179ca0623414879e6b
Jonathan Zempel <jzempel@gmail.com>
2024-04-09 11:26:23 -0400

    feat(theming): fill out color variables (#1782)

--- packages/theming/src/elements/ThemeProvider.spec.tsx
+++ packages/theming/src/elements/ThemeProvider.spec.tsx
@@ -6,8 +6,10 @@
  */
 
 import React from 'react';
+import { useTheme } from 'styled-components';
 import { render } from 'garden-test-utils';
 import { ThemeProvider } from './ThemeProvider';
+import { IGardenTheme } from '../types';
 
 describe('ThemeProvider', () => {
   it('only renders children', () => {
@@ -18,5 +20,43 @@ describe('ThemeProvider', () => {
     );
 
     expect(container.firstChild!.nodeName).toBe('BUTTON');
+  });
+
+  it('provides theme color variable parity between modes', () => {
+    let dark: IGardenTheme['colors']['variables']['dark'] | undefined;
+    let light: IGardenTheme['colors']['variables']['light'] | undefined;
+
+    const Test = () => {
+      const theme = useTheme();
+
+      dark = theme.colors.variables.dark;
+      light = theme.colors.variables.light;
+
+      return <div />;
+    };
+
+    render(<Test />);
+
+    expect(dark).toBeDefined();
+    expect(light).toBeDefined();
+
+    const { background: darkBackground, border: darkBorder, foreground: darkForeground } = dark!;
+    const {
+      background: lightBackground,
+      border: lightBorder,
+      foreground: lightForeground
+    } = light!;
+    const darkKeys = [
+      ...Object.keys(darkBackground),
+      ...Object.keys(darkBorder),
+      ...Object.keys(darkForeground)
+    ].join();
+    const lightKeys = [
+      ...Object.keys(lightBackground),
+      ...Object.keys(lightBorder),
+      ...Object.keys(lightForeground)
+    ].join();
+
+    expect(darkKeys).toStrictEqual(lightKeys);
   });
 });
