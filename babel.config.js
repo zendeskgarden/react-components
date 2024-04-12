5c73cad3e9eb8882db991b931b4e25650572d178
George Treviranus <3946669+geotrev@users.noreply.github.com>
2024-04-12 15:21:57 -0500

    chore: update with `main` (#1788)
    Co-authored-by: Florent <ze-flo@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -10,19 +10,19 @@ module.exports = {
     [
       '@babel/preset-env',
       {
-        targets: '> 0.5%, last 2 versions, Firefox ESR, not dead'
+        // https://browsersl.ist/#q=%3E+0.3%25%2C+last+2+versions%2C+Firefox+ESR%2C+not+dead%2C+not+and_qq+13-14%2C+not+kaios+2-5
+        targets: '> 0.3%, last 2 versions, Firefox ESR, not dead, not and_qq 13-14, not kaios 2-5'
       }
     ],
-    '@babel/preset-react',
+    [
+      '@babel/preset-react',
+      {
+        useBuiltIns: true // https://babeljs.io/docs/babel-preset-react#usebuiltins
+      }
+    ],
     ['@babel/preset-typescript', { onlyRemoveTypeImports: true }]
   ],
-  plugins: [
-    '@babel/plugin-transform-object-assign',
-    ['@babel/plugin-transform-class-properties', { loose: true }],
-    'babel-plugin-styled-components',
-    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-    ['@babel/plugin-transform-private-methods', { loose: true }]
-  ],
+  plugins: ['babel-plugin-styled-components'],
   env: {
     production: {
       plugins: [['react-remove-properties', { properties: [/data-test/u] }]]
