5693bed25ad6ee0749e2be42ba6d0c6897384943
George Treviranus <3946669+geotrev@users.noreply.github.com>
2024-04-18 09:34:45 -0500

    chore: removes base icon attrs
    Co-authored-by: Jonathan Zempel <jzempel@gmail.com>

--- packages/theming/src/utils/StyledBaseIcon.ts
+++ packages/theming/src/utils/StyledBaseIcon.ts
@@ -8,14 +8,9 @@
 import styled from 'styled-components';
 import React, { Children } from 'react';
 
-const COMPONENT_ID = 'icon.base';
-
 // eslint-disable-next-line @typescript-eslint/no-unused-vars,garden-local/require-default-theme
 export const StyledBaseIcon = styled(({ children, theme, ...props }) =>
   React.cloneElement(Children.only(children), props)
-).attrs({
-  'data-garden-id': COMPONENT_ID,
-  'data-garden-version': PACKAGE_VERSION
-})`
+)`
   /* stylelint-disable no-empty-block */
 `;
