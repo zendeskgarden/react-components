c77f77771c4e299fd04830a02a6a030ed5fcbc52
george treviranus <geowtrev@gmail.com>
2024-04-29 13:20:21 -0500

    fix(forms): updates Message to receive margin when intermediaries are present

--- packages/datepickers/demo/stories/DatepickerStory.tsx
+++ packages/datepickers/demo/stories/DatepickerStory.tsx
@@ -8,28 +8,45 @@
 import React from 'react';
 import { Story } from '@storybook/react';
 import { Col, Grid, Row } from '@zendeskgarden/react-grid';
-import { Field, Input, Label } from '@zendeskgarden/react-forms';
+import { Field, Input, Label, Message } from '@zendeskgarden/react-forms';
 import { Datepicker, IDatepickerProps } from '@zendeskgarden/react-datepickers';
 import { DATE_STYLE } from './types';
 
 interface IArgs extends IDatepickerProps {
   dateStyle: DATE_STYLE;
+  message?: string;
+  validation?: 'success' | 'warning' | 'error';
+  validationLabel?: string;
 }
 
-export const DatepickerStory: Story<IArgs> = ({ dateStyle, isCompact, ...args }) => {
+export const DatepickerStory: Story<IArgs> = ({
+  dateStyle,
+  isCompact,
+  message,
+  validation,
+  validationLabel,
+  ...args
+}) => {
   const formatDate = (date: Date) =>
     new Intl.DateTimeFormat(args.locale, { dateStyle }).format(date);
 
   return (
     <Grid>
       <Row style={{ height: 'calc(100vh - 80px)' }}>
-        <Col textAlign="center" alignSelf="center">
-          <Field>
-            <Label hidden>{Datepicker.displayName}</Label>
-            <Datepicker {...args} formatDate={formatDate} isCompact={isCompact}>
-              <Input isCompact={isCompact} style={{ width: isCompact ? 256 : 320 }} />
-            </Datepicker>
-          </Field>
+        <Col alignSelf="center">
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <Field style={{ width: isCompact ? 256 : 320 }}>
+              <Label hidden>{Datepicker.displayName}</Label>
+              <Datepicker {...args} formatDate={formatDate} isCompact={isCompact}>
+                <Input isCompact={isCompact} />
+              </Datepicker>
+              {message && (
+                <Message validation={validation} validationLabel={validationLabel}>
+                  {message}
+                </Message>
+              )}
+            </Field>
+          </div>
         </Col>
       </Row>
     </Grid>
