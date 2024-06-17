5457e16f6f6b84fef96851d25a13d555c3e38cfc
george treviranus <geowtrev@gmail.com>
2024-06-17 10:56:03 -0500

    chore: use new solid colors for borders

--- packages/tables/src/styled/StyledRow.ts
+++ packages/tables/src/styled/StyledRow.ts
@@ -52,19 +52,13 @@ const colorStyles = ({
   const selectedBorderColor = getColor({
     variable: 'border.primaryEmphasis',
     light: { offset: -400 },
-    dark: { offset: 200 },
+    dark: { offset: 300 },
     theme
   });
   const hoveredSelectedBackgroundColor = getColor({
     variable: 'background.primaryEmphasis',
     transparency: theme.opacity[300],
     dark: { offset: -100 },
-    theme
-  });
-  const hoveredSelectedBorderColor = getColor({
-    variable: 'border.primaryEmphasis',
-    light: { offset: -300 },
-    dark: { offset: 100 },
     theme
   });
   const boxShadowColor = getColor({ variable: 'border.primaryEmphasis', theme });
@@ -77,13 +71,12 @@ const colorStyles = ({
   if ($isSelected) {
     if ($isHovered) {
       backgroundColor = hoveredSelectedBackgroundColor;
-      borderColor = hoveredSelectedBorderColor;
     } else {
       backgroundColor = selectedBackgroundColor;
-      borderColor = selectedBorderColor;
     }
 
-    hoverBorderBottomColor = hoveredSelectedBorderColor;
+    borderColor = selectedBorderColor;
+    hoverBorderBottomColor = selectedBorderColor;
     hoverBackgroundColor = hoveredSelectedBackgroundColor;
   } else if ($isHovered) {
     backgroundColor = hoveredBackgroundColor;
