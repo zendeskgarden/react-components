7503a7203aa2a20b5a993d0101fe7426f6f2ad57
Florent Mathieu <florent.mathieu@zendesk.com>
2024-07-16 15:01:51 -1000

    tests: add specs

---
+++ packages/colorpickers/src/styled/common/StyledRange.spec.tsx
@@ -0,0 +1,57 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { getRenderFn } from 'garden-test-utils';
+import { StyledRange } from './StyledRange';
+import { PALETTE } from '@zendeskgarden/react-theming';
+
+describe('StyledRange', () => {
+  it.each<['light' | 'dark', string]>([
+    ['light', PALETTE.white],
+    ['dark', PALETTE.grey[1100]]
+  ])('renders %s mode thumb background color', (mode, color) => {
+    const { container } = getRenderFn(mode)(<StyledRange />);
+
+    expect(container.firstChild).toHaveStyleRule('background-color', color, {
+      modifier: '&::-webkit-slider-thumb'
+    });
+  });
+
+  it.each<['light' | 'dark', string]>([
+    ['light', PALETTE.grey[100]],
+    ['dark', PALETTE.grey[1000]]
+  ])('renders %s mode thumb hover background color', (mode, color) => {
+    const { container } = getRenderFn(mode)(<StyledRange />);
+
+    expect(container.firstChild).toHaveStyleRule('background-color', color, {
+      modifier: ':hover::-webkit-slider-thumb'
+    });
+  });
+
+  it.each<['light' | 'dark', string]>([
+    ['light', PALETTE.white],
+    ['dark', PALETTE.grey[1100]]
+  ])('renders %s mode thumb active background color', (mode, color) => {
+    const { container } = getRenderFn(mode)(<StyledRange />);
+
+    expect(container.firstChild).toHaveStyleRule('background-color', color, {
+      modifier: ':active::-webkit-slider-thumb'
+    });
+  });
+
+  it.each<['light' | 'dark', string]>([
+    ['light', PALETTE.white],
+    ['dark', PALETTE.grey[1100]]
+  ])('renders %s mode thumb focus-visible background color', (mode, color) => {
+    const { container } = getRenderFn(mode)(<StyledRange />);
+
+    expect(container.firstChild).toHaveStyleRule('background-color', color, {
+      modifier: ':focus-visible::-webkit-slider-thumb'
+    });
+  });
+});
