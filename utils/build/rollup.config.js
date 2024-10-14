123592b6ff236025dbddbcaaa6c90adcb11c05e5
Jonathan Zempel <jzempel@gmail.com>
2024-10-14 11:15:40 -0400

    Remove size snapshots

--- utils/build/rollup.config.js
+++ utils/build/rollup.config.js
@@ -13,7 +13,6 @@ import replace from '@rollup/plugin-replace';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript2';
 import { babel } from '@rollup/plugin-babel';
-// import { sizeSnapshot } from '@brodybits/rollup-plugin-size-snapshot';
 import analyze from 'rollup-plugin-analyzer';
 import cleanup from 'rollup-plugin-cleanup';
 import del from 'rollup-plugin-delete';
@@ -84,13 +83,6 @@ export default [
        * Remove comments from source files
        */
       cleanup({ extensions: ['js', 'jsx', 'ts', 'tsx'] }),
-      /**
-       * Only enforce matching size snapshot files in CI environments
-       */
-      // sizeSnapshot({
-      //   matchSnapshot: !!process.env.CI,
-      //   printInfo: !!process.env.CI || !!process.env.ANALYZE_BUNDLE
-      // }),
       !!process.env.ANALYZE_BUNDLE && analyze({ summaryOnly: true })
     ],
     output: [
