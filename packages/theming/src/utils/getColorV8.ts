4dd26c837ca1840eb03fc316c2b5edfd8e07e76c
Jonathan Zempel <jzempel@gmail.com>
2024-10-23 17:51:47 -0400

    fix(theming): `getColor` memoization peformance

--- packages/theming/src/utils/getColorV8.ts
+++ packages/theming/src/utils/getColorV8.ts
@@ -25,6 +25,54 @@ const adjust = (color: string, expected: number, actual: number) => {
   return color;
 };
 
+const CACHE = new WeakMap();
+const KEYS = { colors: 0, palette: 0 };
+
+CACHE.set(DEFAULT_THEME.colors, KEYS.colors);
+CACHE.set(DEFAULT_THEME.palette, KEYS.palette);
+
+const toKey = ({
+  hue,
+  shade,
+  theme,
+  transparency
+}: {
+  hue: Hue;
+  shade?: number;
+  theme?: DefaultTheme;
+  transparency?: number;
+}) => {
+  let retVal = `${hue}`;
+
+  if (shade !== undefined) {
+    retVal += `,${shade}`;
+  }
+
+  if (theme !== undefined) {
+    let themeColorsKey = CACHE.get(theme.colors);
+
+    if (themeColorsKey === undefined) {
+      themeColorsKey = ++KEYS.colors;
+      CACHE.set(theme.colors, themeColorsKey);
+    }
+
+    let themePaletteKey = CACHE.get(theme.palette);
+
+    if (themePaletteKey === undefined) {
+      themePaletteKey = ++KEYS.palette;
+      CACHE.set(theme.palette, themePaletteKey);
+    }
+
+    retVal += `,{${themeColorsKey},${themePaletteKey}}`;
+  }
+
+  if (transparency !== undefined) {
+    retVal += `,${transparency}`;
+  }
+
+  return retVal;
+};
+
 /**
  * @deprecated Use `getColor` instead.
  *
@@ -96,6 +144,5 @@ export const getColorV8 = memoize(
 
     return retVal;
   },
-  (hue, shade, theme, transparency) =>
-    JSON.stringify({ hue, shade, palette: theme?.palette, colors: theme?.colors, transparency })
+  (hue, shade, theme, transparency) => toKey({ hue, shade, theme, transparency })
 );
