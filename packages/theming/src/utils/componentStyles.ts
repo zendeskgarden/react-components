6021148e3242a3d05dff178bc6584f07683ecaca
Jonathan Zempel <jzempel@gmail.com>
2024-12-04 11:00:10 -0500

    Add unit testing

--- packages/theming/src/utils/componentStyles.ts
+++ packages/theming/src/utils/componentStyles.ts
@@ -11,8 +11,8 @@ import { DataAttributes, DefaultTheme, ThemeProps } from 'styled-components';
  * CSS for component customizations based on `theme.components[componentId]`
  * where `componentId` is provided by the `data-garden-id` attribute.
  *
- * @param {Object} options.theme Provides `components` object use to resolve the given component ID
- * @param {String} [options.componentId] Specifies the lookup id for * `theme.components` styles.
+ * @param {Object} props.theme Provides `components` object use to resolve the given component ID
+ * @param {String} [props.componentId] Specifies the lookup id for * `theme.components` styles.
  *  The ID will be inferred from the `'data-garden-id'` attribute if not provided.
  *
  * @returns component CSS styles
