e06ab51773d3deec878ec447dc5a968db5a6945c
Jonathan Zempel <jzempel@gmail.com>
2024-12-06 10:22:44 -0500

    Update JS doc

--- packages/theming/src/utils/componentStyles.ts
+++ packages/theming/src/utils/componentStyles.ts
@@ -8,8 +8,7 @@
 import { DataAttributes, DefaultTheme, ThemeProps } from 'styled-components';
 
 /**
- * CSS for component customizations based on `theme.components[componentId]`
- * where `componentId` is provided by the `data-garden-id` attribute.
+ * CSS for component customizations based on `theme.components[componentId]`.
  *
  * @param {Object} props.theme Provides `components` object use to resolve the given component ID
  * @param {String} [props.componentId] Specifies the lookup id for * `theme.components` styles.
