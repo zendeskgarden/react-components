874bbc6843ead3f49ce3d1f9ce809d1049a20ae7
Jonathan Zempel <jzempel@gmail.com>
2025-01-07 15:36:02 -0500

    Enhance `getColor` storybook demo with generated hue info

--- packages/theming/demo/stories/GetColorStory.tsx
+++ packages/theming/demo/stories/GetColorStory.tsx
@@ -8,15 +8,20 @@
 import React from 'react';
 import { StoryFn } from '@storybook/react';
 import styled, { useTheme } from 'styled-components';
+import { opacify } from 'color2k';
 import { IGardenTheme, getCheckeredBackground, getColor } from '@zendeskgarden/react-theming';
+import { Code, LG, MD, Span } from '@zendeskgarden/react-typography';
+import { Anchor } from '@zendeskgarden/react-buttons';
+import { Grid } from '@zendeskgarden/react-grid';
 import { Tag } from '@zendeskgarden/react-tags';
 
-const StyledDiv = styled.div.attrs<{ background: string }>(p => ({
-  style: { background: p.background }
-}))<{ background: string }>`
+const StyledDiv = styled.div.attrs<{ $background: string }>(p => ({
+  style: { background: p.$background }
+}))`
   display: flex;
   align-items: center;
   justify-content: center;
+  min-width: 32px;
   height: 208px;
 `;
 
@@ -34,6 +39,7 @@ interface IColorProps {
 const Color = ({ dark, hue, light, offset, shade, theme, transparency, variable }: IColorProps) => {
   let background;
   let tag;
+  let generatedHue;
 
   try {
     const backgroundColor = getColor({
@@ -57,16 +63,59 @@ const Color = ({ dark, hue, light, offset, shade, theme, transparency, variable 
         {backgroundColor}
       </Tag>
     );
+    generatedHue = [...Array(12).keys()].reduce<Record<number, string>>((retVal, index) => {
+      const _shade = (index + 1) * 100;
+
+      retVal[_shade] = getColor({ theme, hue: opacify(backgroundColor, 1), shade: _shade });
+
+      return retVal;
+    }, {});
+
+    /* eslint-disable-next-line no-console */
+    console.log(generatedHue);
   } catch (error) {
     background = 'transparent';
     tag = (
-      <Tag hue="red" size="large">
+      <Tag hue="dangerHue" size="large">
         {error instanceof Error ? error.message : String(error)}
       </Tag>
     );
   }
 
-  return <StyledDiv background={background}>{tag}</StyledDiv>;
+  return (
+    <>
+      <StyledDiv $background={background}>{tag}</StyledDiv>
+      {!!generatedHue && (
+        <>
+          <LG style={{ marginTop: 20 }}>Generated hue</LG>
+          <MD style={{ marginBottom: 12 }}>
+            <Span hue="foreground.subtle">
+              Not used when <Code>hue[shade]</Code> is defined by the theme{' '}
+              <Anchor href="https://garden.zendesk.com/components/theme-object#palette" isExternal>
+                palette
+              </Anchor>
+            </Span>
+          </MD>
+          <Grid gutters={false}>
+            <Grid.Row wrap="nowrap">
+              {Object.values(generatedHue).map((backgroundColor, index) => (
+                <Grid.Col key={index}>
+                  <StyledDiv $background={backgroundColor}>
+                    <Tag
+                      hue={getColor({ theme, variable: 'background.default' })}
+                      style={{ position: 'absolute', transform: 'rotate(-90deg)' }}
+                    >
+                      {backgroundColor}
+                    </Tag>
+                  </StyledDiv>
+                </Grid.Col>
+              ))}
+            </Grid.Row>
+          </Grid>
+        </>
+      )}
+    </>
+  );
 };
 
 interface IArgs extends Omit<IColorProps, 'theme'> {
