37b483e8132a1763a3172c3018090be4384b8d66
Jonathan Zempel <jzempel@gmail.com>
2025-08-20 09:08:44 -0400

    Add `Kbd` unit testing

---
+++ packages/typography/src/elements/Kbd.spec.tsx
@@ -0,0 +1,50 @@
+/**
+ * Copyright Zendesk, Inc.
+ *
+ * Use of this source code is governed under the Apache License, Version 2.0
+ * found at http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+import React from 'react';
+import { render, renderRtl } from 'garden-test-utils';
+import { Kbd } from './Kbd';
+
+describe('Kbd', () => {
+  it('renders the expected element', () => {
+    const { container } = render(<Kbd />);
+
+    expect(container.firstChild!.nodeName).toBe('KBD');
+  });
+
+  it('forces left-to-right text direction', () => {
+    const { container } = renderRtl(<Kbd />);
+
+    expect(container.firstChild).toHaveStyleRule('direction', 'ltr');
+  });
+
+  describe('size', () => {
+    it('renders inherited size', () => {
+      const { container } = render(<Kbd />);
+
+      expect(container.firstChild).not.toHaveStyleRule('font-size', 'inherit');
+    });
+
+    it('renders small styling if provided', () => {
+      const { container } = render(<Kbd size="small" />);
+
+      expect(container.firstChild).toHaveStyleRule('font-size', '11px');
+    });
+
+    it('renders medium styling if provided', () => {
+      const { container } = render(<Kbd size="medium" />);
+
+      expect(container.firstChild).toHaveStyleRule('font-size', '13px');
+    });
+
+    it('renders large styling if provided', () => {
+      const { container } = render(<Kbd size="large" />);
+
+      expect(container.firstChild).toHaveStyleRule('font-size', '17px');
+    });
+  });
+});
