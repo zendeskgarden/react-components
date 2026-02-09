6ae846ea098a82cb280160cea20a1160a8d8b527
Jonathan Zempel <jzempel@gmail.com>
2026-02-09 16:37:09 -0500

    fix(dropdowns) non-editable multiselectable `Combobox` behavior

--- packages/dropdowns/src/elements/combobox/TagGroup.tsx
+++ packages/dropdowns/src/elements/combobox/TagGroup.tsx
@@ -5,35 +5,48 @@
  * found at http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-import React, { PropsWithChildren } from 'react';
+import React, { PropsWithChildren, useEffect, useRef } from 'react';
 import { Tag } from './Tag';
 import { ITagGroupProps } from '../../types';
 
 export const TagGroup = ({
   children,
   isDisabled,
+  isEditable,
   isExpanded,
   listboxZIndex,
   maxTags,
   optionTagProps,
   selection
-}: PropsWithChildren<ITagGroupProps>) => (
-  <>
-    {selection.map((option, index) => {
-      const disabled = isDisabled || option.disabled;
+}: PropsWithChildren<ITagGroupProps>) => {
+  const lastTagRef = useRef<HTMLDivElement>(null);
 
-      return (
-        <Tag
-          key={option.value}
-          hidden={!isExpanded && index >= maxTags}
-          option={{ ...option, disabled }}
-          tooltipZIndex={listboxZIndex ? listboxZIndex + 1 : undefined}
-          {...optionTagProps[option.value]}
-        />
-      );
-    })}
-    {children}
-  </>
-);
+  useEffect(() => {
+    if (!isEditable) {
+      // Scroll the last tag into view.
+      lastTagRef.current?.scrollIntoView?.();
+    }
+  }, [isEditable, selection]);
+
+  return (
+    <>
+      {selection.map((option, index) => {
+        const disabled = isDisabled || option.disabled;
+
+        return (
+          <Tag
+            key={option.value}
+            hidden={!isExpanded && index >= maxTags}
+            option={{ ...option, disabled }}
+            tooltipZIndex={listboxZIndex ? listboxZIndex + 1 : undefined}
+            {...optionTagProps[option.value]}
+            ref={index === selection.length - 1 ? lastTagRef : undefined}
+          />
+        );
+      })}
+      {children}
+    </>
+  );
+};
 
 TagGroup.displayName = 'TagGroup';
