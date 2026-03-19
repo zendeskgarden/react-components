1c11386b3fc6118b251568f55b04814be368e9df
Jonathan Zempel <jzempel@gmail.com>
2026-03-19 16:47:00 -0400

    chore: add data attributes for accordion label component

--- src/elements/accordion/Label.tsx
+++ src/elements/accordion/Label.tsx
@@ -10,6 +10,7 @@ import { ButtonHTMLAttributes, forwardRef } from 'react';
 import { useAccordionContext } from '../../hooks/accordion/useAccordionContext';
 import { useHeaderContext } from '../../hooks/accordion/useHeaderContext';
 import { StyledLabel } from '../../views/accordion/StyledLabel';
+import { COMPONENT_IDS } from '../utils';
 
 /**
  * @extends ButtonHTMLAttributes<HTMLButtonElement>
@@ -27,6 +28,8 @@ export const Label = forwardRef<HTMLButtonElement, ButtonHTMLAttributes<HTMLButt
           ...(buttonProps as any) /* HACK to workaround https://github.com/styled-components/styled-components/issues/5652 */
         }
         {...props}
+        data-garden-id={COMPONENT_IDS['accordions.button']}
+        data-garden-version={PACKAGE_VERSION}
         ref={ref}
       />
     );
